33fad0923fca0a1afc5550ee14648047dbf05bba
Vengadesh <vengadesh@emdsys.com>
2018-05-24 18:47:32 +0530

    EXA-9470 - Billing 1.5 Report Layout

--- app/js/routes/app.router.js
+++ app/js/routes/app.router.js
@@ -1,16 +1,23 @@
 define([
     'backbone',
-    'backbonesubroute',
-    'views/worklist'
-], function (Backbone, BackboneSubroute, WorklistView) {
+    'views/worklist',
+    'modules/reporting/views/billing/charges'
+], function (Backbone, WorklistView, ReportView) {
     var AppRouter = Backbone.Router.extend({
         routes: {
-            "app/worklist": "startApp"
+            "app/worklist": "startApp" ,
+            "app/report": "startReporting" 
         },
 
         startApp: function (subroutes) {
             if (!this.appRoute) {
                 this.appRoute = new WorklistView({ el: $('#root') });
+            }
+        },
+        startReporting: function (subroutes) {
+            if (!this.reportingRoute) {
+               // this.reportingRoute = new ReportView("reports/", { createTrailingSlashRoutes: true });
+                this.reportingRoute = new ReportView({ el: $('#root') });
             }
         }
     });
