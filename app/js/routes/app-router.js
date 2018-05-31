b4f7ee92f2049354510739c0a082626646e56e0b
Vengadesh <vengadesh@emdsys.com>
2018-05-31 19:19:04 +0530

    EXA-9470 - Billing 1.5 Report Layout (Missing File Added)

--- app/js/routes/app-router.js
+++ app/js/routes/app-router.js
@@ -6,12 +6,21 @@ define([
     'routes/app/index',
     'routes/setup/index',
     'modules/reporting/views/billing/charges',
-    'modules/reporting/views/billing/claim_activity',
-    'modules/reporting/views/billing/claim_inquiry',
-    'modules/reporting/views/billing/patient_statement',
-    'modules/reporting/views/billing/modality_summary',
-    'modules/reporting/views/billing/payer_mix',
+    'modules/reporting/views/billing/claim-activity',
+    'modules/reporting/views/billing/claim-inquiry',
+    'modules/reporting/views/billing/patient-statement',
+    'modules/reporting/views/billing/modality-summary',
+    'modules/reporting/views/billing/payer-mix',
     'modules/reporting/views/billing/payments',
+    'modules/reporting/views/billing/claim-activity',
+    'modules/reporting/views/billing/claim-inquiry',
+    'modules/reporting/views/billing/patient-statement',
+    'modules/reporting/views/billing/modality-summary',
+    'modules/reporting/views/billing/payer-mix',
+    'modules/reporting/views/billing/payments-by-ins-company',
+    'modules/reporting/views/billing/referring-provider-count',
+    'modules/reporting/views/billing/referring-provider-summary',
+    'modules/reporting/views/billing/transaction-summary',
     'views/app/payments',
     'views/app/payment-edit'
 ], function (Backbone,
@@ -27,6 +36,15 @@ define([
     PatientStatementView, 
     MoadalitySummaryView, 
     PayerMixView,
+    ClaimActivityView, 
+    ClaimInquiryView, 
+    PatientStatementView, 
+    MoadalitySummaryView, 
+    PayerMixView,
+    PaymentByInsCompanyView,
+    ReferringProviderCountView,
+    ReferringProviderSummaryView,
+    transactionSummaryView,
     PaymentsView,
     EditPaymentView
     ) {
@@ -43,50 +61,52 @@ define([
                 "app/reports/patient_statement": "startPatientStatementReporting",
                 "app/reports/modality_summary": "startModalitySummaryReporting",           
                 "app/reports/payer_mix": "startPayerMixReporting",
+                "app/reports/claim-activity": "startClaimActivityReporting",
+                "app/reports/claim-inquiry": "startClaimInquiryReporting",
+                "app/reports/patient-statement": "startPatientStatementReporting",
+                "app/reports/modality-summary": "startModalitySummaryReporting",           
+                "app/reports/payer-mix": "startPayerMixReporting",
+                "app/reports/payments-by-ins-company": "startPaymentsByInsuranceCompanyReporting",
+                "app/reports/referring-provider-count": "startReferringProviderCountReporting",
+                "app/reports/referring-provider-summary": "startReferringProviderSummaryReporting",
+                "app/reports/transaction-summary": "starttransactionSummaryReporting",
     
                 // "app/*subroute": "startApp",
                 "setup/*subroute": "startSetup",
                 "app/payments": "startPayments",
                 "app/payments/edit/:id": "editPayment"
             },
-
             // startApp: function (subroutes) {
             //     if (!this.appRoute) {
             //         this.appRoute = new WorklistView({ el: $('#root') });
             //     }
             // },
-
             startApp: function (subroute) {
                 if (!this.appRouter) {
                     this.appRouter = new AppRoute("app/", { createTrailingSlashRoutes: true });
                 }
             },
-
             startSetup: function (subroute) {
                 if (!this.setupRouter) {
                     this.setupRouter = new SetupRoute("setup/", { createTrailingSlashRoutes: true });
                 }
             },
-
             startReporting: function (subroutes) {
                 if (!this.reportingRoute) {
                     // this.reportingRoute = new ReportView("reports/", { createTrailingSlashRoutes: true });
                     this.reportingRoute = new ReportView({ el: $('#root') });
                 }
             },
-
             startAppStudies: function (subroutes) {
                 if (!this.appRoute) {
                     this.appRoute = new StudiesView({ el: $('#root') });
                 }
             },
-
             startClaimWorkBench: function (subroutes) {
                 if (!this.appClaimWorkBenchRoute) {
                     this.appClaimWorkBenchRoute = new ClaimWorkBenchView({ el: $('#root') });
                 }
             },
-
             startChargeReporting: function (subroutes) {
                 if (!this.reportingRoute) {             
                     this.reportingRoute = new ChargeReportView({ el: $('#root') });
@@ -118,14 +138,57 @@ define([
                 if (!this.reportingRoute) {             
                     this.reportingRoute = new MoadalitySummaryView({ el: $('#root') });
                 }
-            }, 
-            
+            },             
             startPayerMixReporting: function (subroutes) {
                 if (!this.reportingRoute) {             
                     this.reportingRoute = new PayerMixView({ el: $('#root') });
                 }
             },
-
+            startClaimActivityReporting: function (subroutes) {
+                if (!this.reportingRoute) {             
+                    this.reportingRoute = new ClaimActivityView({ el: $('#root') });
+                }
+            },
+            startClaimInquiryReporting: function (subroutes) {
+                if (!this.reportingRoute) {             
+                    this.reportingRoute = new ClaimInquiryView({ el: $('#root') });
+                }
+            },
+            startPatientStatementReporting: function (subroutes) {
+                if (!this.reportingRoute) {             
+                    this.reportingRoute = new PatientStatementView({ el: $('#root') });
+                }
+            },
+            startModalitySummaryReporting: function (subroutes) {
+                if (!this.reportingRoute) {             
+                    this.reportingRoute = new MoadalitySummaryView({ el: $('#root') });
+                }
+            },             
+            startPayerMixReporting: function (subroutes) {
+                if (!this.reportingRoute) {             
+                    this.reportingRoute = new PayerMixView({ el: $('#root') });
+                }
+            },
+            startPaymentsByInsuranceCompanyReporting: function (subroutes) {
+                if (!this.reportingRoute) {             
+                    this.reportingRoute = new PaymentByInsCompanyView({ el: $('#root') });
+                }
+            },            
+            startReferringProviderCountReporting: function (subroutes) {
+                if (!this.reportingRoute) {             
+                    this.reportingRoute = new ReferringProviderCountView({ el: $('#root') });
+                }
+            },
+            startReferringProviderSummaryReporting: function (subroutes) {
+                if (!this.reportingRoute) {             
+                    this.reportingRoute = new ReferringProviderSummaryView({ el: $('#root') });
+                }
+            },
+            starttransactionSummaryReporting: function (subroutes) {
+                if (!this.reportingRoute) {             
+                    this.reportingRoute = new transactionSummaryView({ el: $('#root') });
+                }
+            },
             startPayments: function (subroutes) {
                 if (!this.appRoute) {
                     this.appRoute = new PaymentsView({ el: $('#root') });
@@ -136,14 +199,11 @@ define([
                     this.appRoute = new EditPaymentView({ el: $('#root'), id: paymentId });
                 }
             },
-
             initialize: function () {
                 $('#initialLoading').hide();
                 $('#root-content').show();
             }
-
         });
-
         return {
             initialize: function () {
                 new AppRouter();
@@ -151,4 +211,3 @@ define([
             }
         };
     });
-    
