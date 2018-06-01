803da22dab560a77b9b7a91e9397a80a21844bc0
RameshPrabhakar <ramesh.prabhakar@emd.sys.com>
2018-06-01 18:08:45 +0530

    line space between functions

--- server/controllers/claims.js
+++ server/controllers/claims.js
@@ -1,5 +1,6 @@
 const data = require('../data/claims');
 const Promise = require('bluebird');
+
 module.exports = {
 
     getLineItemsDetails: (params) => { return data.getLineItemsDetails(params); },
@@ -21,16 +22,25 @@ module.exports = {
     getMasterDetails: (params) => { return data.getMasterDetails(params); },
     
     save: async (params) => { 
+
         charges(params);
+
         async function charges(objects) {
+
             const results = [];
             const claimResult = await data.save(params);
+
             for (const obj of objects.charges) {
+
                 if(claimResult.rows.length && claimResult.rows[0]){
+
                     obj.claim_id = claimResult.rows[0].id;
+
                 }
+                
                 results.push(data.saveCharges(obj));
             }
+
             return await Promise.all(results);
         }
     }
