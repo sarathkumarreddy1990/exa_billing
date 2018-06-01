3e8cb8f1b1bd72659ffaa09a76f5e6e7d9939721
Eswaraprasad <eswaraprasad@emdsys.com>
2018-06-01 16:11:30 +0530

    EXA-9526   Billing1.5 API for Billing provider_id_codes apis EXA-9527   Billing1.5 API for Billing provider_id_code_qualifiers apis EXA-9525   Billing1.5 API for Billing providers delete api changes

--- server/routes/setup/index.js
+++ server/routes/setup/index.js
@@ -3,9 +3,13 @@ const casGroupRouter = require('./cas-group-codes');
 const adjustmentRouter = require('./adjustment-codes');
 const casReasonRouter = require('./cas-reason-codes');
 const billingProviderRouter = require('./billing-providers');
+const providerIdCodeQualifierRouter = require('./provider-id-code-qualifiers');
+const providerIdCodeRouter = require('./provider-id-codes');
 
 const app = module.exports = express();
 app.use('/cas_group_codes',casGroupRouter);
 app.use('/adjustment_codes',adjustmentRouter);
 app.use('/cas_reason_codes', casReasonRouter);
 app.use('/billing_providers',billingProviderRouter);
+app.use('/provider_id_code_qualifiers',providerIdCodeQualifierRouter);
+app.use('/provider_id_codes/',providerIdCodeRouter);
