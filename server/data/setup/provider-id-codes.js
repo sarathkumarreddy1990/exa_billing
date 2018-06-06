90df47d155eaa549f230f6052122c277cea43576
Eswaraprasad <eswaraprasad@emdsys.com>
2018-06-06 18:07:07 +0530

    billing provider api changes and test api changes

--- server/data/setup/provider-id-codes.js
+++ server/data/setup/provider-id-codes.js
@@ -7,21 +7,21 @@ module.exports = {
         let { provider_id } = params;
 
         const sql = SQL`SELECT 
-                            pc.id,
-                            pc.qualifier_id
-                            , pc.billing_provider_id
-                            , pc.insurance_provider_id
-                            , ip.insurance_name
-                            , pc.payer_assigned_provider_id
-                            , pcq.description as qualifier_desc
-                            , COUNT(1) OVER (range unbounded preceding) as total_records
-                        FROM   billing.provider_id_codes as pc
-                        LEFT JOIN billing.provider_id_code_qualifiers pcq ON pc.qualifier_id = pcq.id
-                        LEFT JOIN insurance_providers ip ON pc.insurance_provider_id = ip.id
-                        WHERE 
-                            billing_provider_id = ${provider_id}
-                        ORDER  BY  pcq.id DESC 
-                        LIMIT  10  `;
+                            pc.id
+                          , pc.qualifier_id
+                          , pc.billing_provider_id
+                          , pc.insurance_provider_id
+                          , ip.insurance_name
+                          , pc.payer_assigned_provider_id
+                          , pcq.description as qualifier_desc
+                          , COUNT(1) OVER (range unbounded preceding) as total_records
+                        FROM billing.provider_id_codes as pc
+                        INNER JOIN billing.provider_id_code_qualifiers pcq ON pc.qualifier_id = pcq.id
+                        INNER JOIN insurance_providers ip ON pc.insurance_provider_id = ip.id
+                        WHERE
+                             billing_provider_id = ${provider_id}
+                        ORDER BY pc.id DESC 
+                        LIMIT 10`;
 
         return await query(sql);
     },
@@ -34,11 +34,11 @@ module.exports = {
 
         const sql = SQL`SELECT 
                               id
-                              , qualifier_id
-                              , billing_provider_id
-                              , insurance_provider_id
-                              , payer_assigned_provider_id
-                        FROM   billing.provider_id_codes
+                            , qualifier_id
+                            , billing_provider_id
+                            , insurance_provider_id
+                            , payer_assigned_provider_id
+                        FROM billing.provider_id_codes
                         WHERE 
                             id = ${id}
                         AND billing_provider_id = ${provider_id}`;
@@ -50,7 +50,7 @@ module.exports = {
 
         let {
             qualifierId,
-            providerId,
+            provider_id,
             insuranceProviderId,
             payerAssignedProviderId } = params;
 
@@ -62,7 +62,7 @@ module.exports = {
                                                 values
                                                 (
                                                     ${qualifierId}
-                                                  , ${providerId}
+                                                  , ${provider_id}
                                                   , ${insuranceProviderId}
                                                   , ${payerAssignedProviderId}
                                                 ) RETURNING id`;
@@ -75,7 +75,7 @@ module.exports = {
         let {
             id,
             qualifierId,
-            providerId,
+            provider_id,
             insuranceProviderId,
             payerAssignedProviderId } = params;
 
@@ -88,7 +88,7 @@ module.exports = {
                             , payer_assigned_provider_id = ${payerAssignedProviderId}
                          WHERE
                             id = ${id}
-                        AND billing_provider_id = ${providerId}`;
+                        AND billing_provider_id = ${provider_id}`;
 
         return await query(sql);
     },
