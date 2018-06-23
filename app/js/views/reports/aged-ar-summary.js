a1101cf0f5955a6ca3e1c2585d1d361eb3c0dabe
Vengadesh <vengadesh@emdsys.com>
2018-06-23 16:23:41 +0530

    EXA-9851 - Aged AR Summary Report - Billing 1.5

---
+++ app/js/views/reports/aged-ar-summary.js
@@ -0,0 +1,165 @@
+define([
+    'jquery'
+    , 'underscore'
+    , 'backbone'
+    , 'shared/report-utils'
+    , 'text!templates/reports/aged-ar-summary.html',
+],
+    function ($, _, Backbone, UI, MainTemplate) {
+
+        var AgedARSummaryView = Backbone.View.extend({
+            rendered: false,
+            dtpEndMonth: null,
+            expanded: false,
+            mainTemplate: _.template(MainTemplate),
+            viewModel: {
+                facilities: null,
+                allFacilities: false,
+                facilityIds: null,
+                openInNewTab: false,
+                reportId: null,
+                reportCategory: null,
+                reportTitle: null,
+                reportFormat: null,
+                billingProvider: null,
+                billingProviders: null,
+                allBillingProvider: false,
+                excelExtended: false
+            },
+            selectedBillingProList: [],
+            selectedFacilityList: [],
+            defaultyFacilityId: null,
+            events: {
+                'click #btnViewReport': 'onReportViewClick',
+                'click #btnViewReportNewTab': 'onReportViewClick',
+                'click #btnPdfReport': 'onReportViewClick',
+                'click #btnExcelReport': 'onReportViewClick',
+                'click #btnExcelReportExtended': 'onReportViewClick',
+                'click #btnCsvReport': 'onReportViewClick',
+                'click #btnXmlReport': 'onReportViewClick',
+            },
+
+            initialize: function (options) {
+                this.showForm();
+                this.$el.html(this.mainTemplate(this.viewModel));
+                UI.initializeReportingViewModel(options, this.viewModel);
+
+                // Set date range to Facility Date
+                this.viewModel.dateFrom = commonjs.getFacilityCurrentDateTime(app.facilityID);
+                this.viewModel.dateTo = this.viewModel.dateFrom.clone();
+            },
+
+            showForm: function () {
+                if (!this.rendered) {
+                    this.render();
+                }
+                commonjs.initializeScreen({ header: { screen: this.viewModel.reportTitle, ext: this.viewModel.reportId } }); // html title
+                UI.setPageTitle(this.viewModel.reportTitle);
+            },
+
+            render: function () {
+                var modelCollection = Backbone.Collection.extend({
+                    model: Backbone.Model.extend({})
+                });
+                this.viewModel.facilities = new modelCollection(commonjs.getCurrentUsersFacilitiesFromAppSettings());   
+                this.$el.html(this.mainTemplate(this.viewModel));
+                // bind DRP and initialize it
+                this.bindDateRangePicker();
+                this.drpStudyDt.setStartDate(this.viewModel.dateFrom);
+                this.drpStudyDt.setEndDate(this.viewModel.dateTo);              
+              
+                UI.bindBillingProvider();
+                $('#ddlFacilityFilter').multiselect({
+                    maxHeight: 200,
+                    buttonWidth: '300px',
+                    width: '300px',
+                    enableFiltering: true,
+                    includeSelectAllOption: true,
+                    enableCaseInsensitiveFiltering: true
+                });
+            },
+
+            onReportViewClick: function (e) {
+                var btnClicked = e && e.target ? $(e.target) : null;
+                this.getSelectedFacility();
+                this.getBillingProvider();
+                if (btnClicked && btnClicked.prop('tagName') === 'I') {
+                    btnClicked = btnClicked.parent(); // in case FA icon 'inside'' button was clicked...
+                }
+                const rFormat = btnClicked ? btnClicked.attr('data-rformat') : null;
+                const openInNewTab = btnClicked ? btnClicked.attr('id') === 'btnViewReportNewTab' : false;
+                this.excelExtended = btnClicked ? btnClicked.attr('id') === 'btnExcelReportExtended' : false;
+                this.viewModel.reportFormat = rFormat;
+                this.viewModel.openInNewTab = (openInNewTab && rFormat === 'html') ? true : false;
+                if (this.hasValidViewModel()) {
+                    const urlParams = this.getReportParams();
+                    UI.showReport(this.viewModel.reportId, this.viewModel.reportCategory, this.viewModel.reportFormat, urlParams, this.viewModel.openInNewTab);
+                }
+            },
+
+            bindDateRangePicker: function () {
+                var self = this;
+                var drpEl = $('#txtDateRangeFromTo');
+                var drpOptions = { autoUpdateInput: true, locale: { format: 'L' } };
+                this.drpStudyDt = commonjs.bindDateRangePicker(drpEl, drpOptions, 'past', function (start, end, format) {
+                    self.viewModel.dateFrom = start;
+                    self.viewModel.dateTo = end;
+                });
+                drpEl.on('cancel.daterangepicker', function (ev, drp) {
+                    self.viewModel.dateFrom = null;
+                    self.viewModel.dateTo = null;
+                });
+            },
+
+            hasValidViewModel: function () {
+                if (this.viewModel.reportId == null || this.viewModel.reportCategory == null || this.viewModel.reportFormat == null) {
+                    commonjs.showWarning('Please check report id, category, and/or format!');
+                    return false;
+                }
+                if (this.viewModel.dateFrom == null || this.viewModel.dateTo == null) {
+                    commonjs.showWarning('Please select date range!');
+                    return;
+                }
+                return true;
+            },
+
+            getSelectedFacility: function (e) {
+                var selected = $("#ddlFacilityFilter option:selected");
+                var facilities = [];
+                selected.each(function () {
+                    facilities.push($(this).val());
+                });
+                this.selectedFacilityList = facilities
+                this.viewModel.allFacilities = this.selectedFacilityList && this.selectedFacilityList.length === $("#ddlFacilityFilter option").length;
+            },
+
+            // multi select billing provider - worked
+            getBillingProvider: function (e) {
+                var billing_pro = []
+                var selected = $("#ddlBillingProvider option:selected");
+                selected.each(function () {
+                    billing_pro.push($(this).val());
+                });
+                this.selectedBillingProList = billing_pro;
+                this.viewModel.allBillingProvider = this.selectedBillingProList && this.selectedBillingProList.length === $("#ddlBillingProvider option").length;
+            },
+
+            getReportParams: function () {
+                const urlParams = {
+                    'facilityIds': this.selectedFacilityList ? this.selectedFacilityList : [],
+                    'allFacilities': this.viewModel.allFacilities ? this.viewModel.allFacilities : '',
+                    'fromDate': this.viewModel.fromDate.date().format('YYYY-MM-DD'),
+                    'billingProvider': this.selectedBillingProList ? this.selectedBillingProList : [],
+                    'allBillingProvider': this.viewModel.allBillingProvider ? this.viewModel.allBillingProvider : '',
+                    billingProFlag: this.viewModel.allBillingProvider == 'true' ? true : false,
+                    'incPatDetail': $('#incPat').prop('checked'),
+                    'excCreditBal': $('#excCreBal').prop('checked'),
+                    'excelExtended': this.excelExtended ? this.excelExtended : ''
+                }
+                return urlParams;
+            }
+
+        });
+
+        return AgedARSummaryView;
+    });
