ab2ac2f84d84f8bdb077d32e59b90550ea151be4
Ramesh R <ramesh@emdsys.com>
2018-06-26 18:32:59 +0530

    Build v1.4.2

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,10 @@ const concat = require('gulp-concat');
 const concatCss = require('gulp-concat-css');
 const requirejs = require('requirejs');
 const zip = require('gulp-zip');
+const bump = require('gulp-bump');
+const git = require('gulp-git');
+
+const fs = require('fs');
 const path = require('path');
 
 let requirejsConfig = require('./app/js/main').rjsConfig;
@@ -28,7 +32,7 @@ gulp.task('less', ['copy'], () => {
             paths: [path.join(__dirname, 'app/skins/default/index.less')]
         }))
         .pipe(gulp.dest('./build/app/skins/default'))
-        //.pipe(gulp.dest('./app/skins/default'))
+    //.pipe(gulp.dest('./app/skins/default'))
 });
 
 /// TODO: Auto prefix
@@ -44,7 +48,7 @@ gulp.task('concat-css', ['less'], () => {
     ])
         .pipe(concat('index.min.css'))
         //.pipe(gulp.dest('./dist/css'))
-         .pipe(gulp.dest('./app/skins/default'))
+        .pipe(gulp.dest('./app/skins/default'))
 });
 
 gulp.task('requirejsBuild', ['less'], (done) => {
@@ -89,9 +93,47 @@ gulp.task('clean-all', ['zip'], () => {
         .pipe(clean());
 });
 
-gulp.task('clean-all', ['zip'], () => {
-    return gulp.src('./build')
-        .pipe(clean());
+gulp.task('bump', ['clean-all'], () => {
+    return gulp.src('./package.json')
+        .pipe(bump({ type: 'patch' }))
+        .pipe(gulp.dest('./'));
+});
+
+gulp.task('bump-release', () => {
+    return gulp.src('./package.json')
+        .pipe(bump({ type: 'minor' }))
+        .pipe(gulp.dest('./'));
+});
+
+gulp.task('git-init', () => {
+    git.init((err) => {
+        if (err) throw err;
+    });
+});
+
+gulp.task('git-add', ['git-init'], () => {
+    return gulp.src('./package.json')
+        .pipe(git.add());
+});
+
+gulp.task('git-commit', ['git-add'], () => {
+    let newVersion;
+
+    //function computeNewVersion() {
+        const package = JSON.parse(fs.readFileSync('./package.json'));
+        newVersion = package.version;
+    //}
+
+    return gulp.src('./package.json')
+        //.pipe(computeNewVersion())
+        .pipe(git.commit(`Build v${newVersion}`));
+        //.pipe(git.commit(() => `Build v${newVersion}`));
+});
+
+gulp.task('git-push', ['git-commit'], () => {
+    git.push('origin', 'develop', (err) => {
+        if (err) throw err;
+    });
 });
 
 gulp.task('build', [
@@ -99,7 +141,8 @@ gulp.task('build', [
     'copy',
     'requirejsBuild',
     'zip',
-    'clean-all'
+    'clean-all',
+    'bump',
 ]);
 
 
