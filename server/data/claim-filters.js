65abbeba30e79491194c53d87dd2d2751d762038
Eswaraprasad <eswaraprasad@emdsys.com>
2018-07-02 16:29:50 +0530

    grid_filters api changes

--- server/data/claim-filters.js
+++ server/data/claim-filters.js
@@ -7,7 +7,7 @@ module.exports = {
         const sqlQuery = (`
                         SELECT id as filter_id,*
                         FROM   billing.grid_filters
-                        WHERE filter_type = 'claims' AND user_id=$1 AND is_active
+                        WHERE filter_type = 'claims' AND user_id=$1 AND inactivated_dt is null
                         order by filter_order  `);
 
         return await query(sqlQuery, [args.userId]);
