df7ecb385091884818bf30da7ec5c1c61d1a5306
Vengadesh <vengadesh@emdsys.com>
2018-07-04 21:06:22 +0530

    EXA-9470 - Billing 1.5 Report changes

--- modules/reporting/data/postgres/billing/patient-statement.js
+++ modules/reporting/data/postgres/billing/patient-statement.js
@@ -102,7 +102,7 @@ WITH claim_data as(
         case when type = 'charge' then amount end as charge,
         case when type = 'payment' then amount end as payment,
         case when type = 'adjustment' then amount end as adjustment,
-        <% if (payToProvider == 'false') {%>
+        <% if (payToProvider == false) {%>
         bp.name as billing_provider_name,
         bp.pay_to_address_line1 as billing_proaddress1,
         bp.pay_to_address_line1 as billing_proaddress2,
@@ -686,7 +686,8 @@ const api = {
             patientIds: null,
             billingProviderIds: null,
             facilityIds: null,
-            statementDate: null
+            statementDate: null,
+            payToProvider:null
            
         };
 
@@ -713,9 +714,11 @@ const api = {
 
         params.push(reportParams.sDate);
         filters.sDate = `$${params.length}::date`;
-        filters.statementDate = `$${params.length}::date`;
+        filters.statementDate = `$${params.length}::date`;  
         
         filters.whereDate = queryBuilder.whereDateInTz(`bc.claim_dt`, `<=`, [params.length], `f.time_zone`);   
+        //filters.whereDate = queryBuilder.whereDateInTz(`CASE  WHEN type = 'charge'THEN  bc.claim_dt WHEN  type = 'payment' THEN bp.accounting_dt ELSE py.accounting_date END`, `<=`, [params.length], `f.time_zone`);
+
         filters.payToProvider = reportParams.payToProvider ;
 
         return {
