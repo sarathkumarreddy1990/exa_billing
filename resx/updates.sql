3e882e3173f82821638471da10917b171ada26cc
murali <murali@emdsys.com>
2018-07-10 12:42:54 +0530

    issue fix - bill fee update

--- resx/updates.sql
+++ resx/updates.sql
@@ -2535,6 +2535,8 @@ BEGIN
 				l_bill_fee_recalculation = FALSE;
 			ELSIF ((p_payer_type = 'secondary_insurance' OR p_payer_type = 'tertiary_insurance') AND p_existing_payer_type = 'primary_insurance') THEN
 				l_bill_fee_recalculation = FALSE;
+            ELSIF (p_payer_type = p_existing_payer_type) THEN
+				l_bill_fee_recalculation = FALSE;
 			END IF;
 		END IF;
 		
