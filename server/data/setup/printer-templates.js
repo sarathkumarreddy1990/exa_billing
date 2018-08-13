7af20f293fece675529f2784e7b5a0c010e6f109
Jayachithra Parthasarathy <jayachithra@emdsys.com>
2018-08-13 20:57:21 +0530

    EXA-10374  -- is_default column added in printer templates

--- server/data/setup/printer-templates.js
+++ server/data/setup/printer-templates.js
@@ -26,13 +26,13 @@ module.exports = {
             whereQuery.push(` template_type ILIKE '%${template_type}%'`);
         }
 
-        const sql = SQL`SELECT 
+        const sql = SQL`SELECT
                           id
                         , name
                         , inactivated_dt
                         , template_type
                         , COUNT(1) OVER (range unbounded preceding) AS total_records
-                    FROM   
+                    FROM
                         billing.printer_templates `;
 
         if (whereQuery.length) {
@@ -53,22 +53,22 @@ module.exports = {
     getDataById: async (params) => {
         const { id } = params;
 
-        const sql = SQL`SELECT 
+        const sql = SQL`SELECT
                           id
                         , name
-                        , left_margin 
-                        , right_margin 
-                        , top_margin 
-                        , bottom_margin 
+                        , left_margin
+                        , right_margin
+                        , top_margin
+                        , bottom_margin
                         , template_content
                         , inactivated_dt
                         , page_width
                         , page_height
                         , template_type
                         , is_default
-                    FROM   
-                        billing.printer_templates 
-                    WHERE 
+                    FROM
+                        billing.printer_templates
+                    WHERE
                         id = ${id} `;
 
         return await query(sql);
@@ -87,16 +87,20 @@ module.exports = {
             width,
             type,
             templateContent,
-            isDefault
+            isDefault,
+            screenName,
+            clientIp,
+            userId
         } = params;
 
         let inactivated_date = isActive ? null : ' now() ';
 
         const sql = SQL`
-                    INSERT INTO billing.printer_templates 
-                    ( 
-                        company_id, 
-                        name, 
+                    WITH create_template AS (
+                        INSERT INTO billing.printer_templates
+                    (
+                        company_id,
+                        name,
                         template_content,
                         inactivated_dt,
                         left_margin,
@@ -107,12 +111,12 @@ module.exports = {
                         page_width,
                         template_type,
                         is_default
-                    ) 
-                    VALUES 
-                    ( 
-                        ${companyId}, 
-                        ${name}, 
-                        ${templateContent}, 
+                    )
+                    VALUES
+                    (
+                        ${companyId},
+                        ${name},
+                        ${templateContent},
                         ${inactivated_date},
                         ${marginLeft},
                         ${marginRight},
@@ -124,12 +128,58 @@ module.exports = {
                         ${isDefault}
                     )
                     RETURNING *, '{}'::jsonb old_values
+                )
+                , update_template AS (
+                    UPDATE billing.printer_templates
+                    SET
+                        is_default = false
+                    WHERE
+                        id != (SELECT id FROM create_template)
+                        AND is_default
+                        AND template_type = ${type}
+                        AND (SELECT is_default FROM create_template)
+                    RETURNING *, '{}'::jsonb old_values
+            )
+            , insert_audit_template AS (
+                SELECT billing.create_audit(
+                      ${companyId}
+                    , 'claims'
+                    , id
+                    , ${screenName}
+                    , 'claims'
+                    , 'Add: New Printer Template ' || ${name} || '   created  '
+                    , ${clientIp}
+                    , json_build_object(
+                        'old_values', COALESCE(old_values, '{}'),
+                        'new_values', (SELECT row_to_json(temp_row)::jsonb - 'old_values'::text FROM (SELECT * FROM create_template limit 1) temp_row)
+                      )::jsonb
+                    , ${userId}
+                  ) AS id
+                FROM create_template
+                WHERE id IS NOT NULL
+            )
+            , update_audit_template AS (
+                SELECT billing.create_audit(
+                      ${companyId}
+                    , 'claims'
+                    , id
+                    , ${screenName}
+                    , 'claims'
+                    , 'Printer Template (' || update_template.name ||  ') Updated  to Default '
+                    , ${clientIp}
+                    , json_build_object(
+                        'old_values', COALESCE(old_values, '{}'),
+                        'new_values', (SELECT row_to_json(temp_row)::jsonb - 'old_values'::text FROM (SELECT * FROM update_template limit 1 ) temp_row)
+                      )::jsonb
+                    , ${userId}
+                  ) AS id
+                FROM update_template
+                WHERE id IS NOT NULL
+            )
+            SELECT * FROM insert_audit_template UNION SELECT * FROM update_audit_template
         `;
 
-        return await queryWithAudit(sql, {
-            ...params,
-            logDescription: `Add: New Printer Template(${name}) created`
-        });
+        return await query(sql);
     },
 
     update: async (params) => {
@@ -147,14 +197,19 @@ module.exports = {
             width,
             type,
             templateContent,
-            isDefault
+            isDefault,
+            companyId,
+            screenName,
+            clientIp,
+            userId
         } = params;
 
         let inactivated_date = isActive ? null : ' now() ';
 
-        let sql = SQL`UPDATE
-                             billing.printer_templates 
-                        SET  
+        let sql = SQL` WITH update_template AS (
+                        UPDATE
+                             billing.printer_templates
+                        SET
                                 name = ${name}
                               , template_content = ${templateContent}
                               , inactivated_dt = ${inactivated_date}
@@ -167,20 +222,66 @@ module.exports = {
                               , template_type = ${type}
                               , is_default = ${isDefault}
                         WHERE
-                            id = ${id} 
+                            id = ${id}
                         RETURNING *,
                             (
-                                SELECT row_to_json(old_row) 
-                                FROM   (SELECT * 
-                                        FROM   billing.printer_templates 
-                                        WHERE  id = ${id}) old_row 
+                                SELECT row_to_json(old_row)
+                                FROM   (SELECT *
+                                        FROM   billing.printer_templates
+                                        WHERE  id = ${id}) old_row
                             ) old_values
+                        )
+                        , update_default AS (
+                            UPDATE billing.printer_templates
+                            SET
+                                is_default = false
+                            WHERE
+                                id != ${id}
+                                AND is_default
+                                AND template_type = ${type}
+                                AND ( SELECT is_default FROM update_template )
+                            RETURNING * ,'{}'::jsonb old_values
+                        )
+                        , update_audit_template AS (
+                            SELECT billing.create_audit(
+                                  ${companyId}
+                                , 'claims'
+                                , id
+                                , ${screenName}
+                                , 'claims'
+                                , 'Printer Template Updated  ' || update_template.name
+                                , ${clientIp}
+                                , json_build_object(
+                                    'old_values', COALESCE(old_values, '{}'),
+                                    'new_values', (SELECT row_to_json(temp_row)::jsonb - 'old_values'::text FROM (SELECT * FROM update_template limit 1) temp_row)
+                                  )::jsonb
+                                , ${userId}
+                              ) AS id
+                            FROM update_template
+                            WHERE id IS NOT NULL
+                        )
+                        , update_audit_template_default AS (
+                            SELECT billing.create_audit(
+                                  ${companyId}
+                                , 'claims'
+                                , id
+                                , ${screenName}
+                                , 'claims'
+                                , 'Printer Template (' || update_template.name ||  ') Updated  to Default '
+                                , ${clientIp}
+                                , json_build_object(
+                                    'old_values', COALESCE(old_values, '{}'),
+                                    'new_values', (SELECT row_to_json(temp_row)::jsonb - 'old_values'::text FROM (SELECT * FROM update_default limit 1 ) temp_row)
+                                  )::jsonb
+                                , ${userId}
+                              ) AS id
+                            FROM update_template
+                            WHERE id IS NOT NULL
+                        )
+                        SELECT * FROM update_audit_template UNION SELECT * FROM update_audit_template_default
                     `;
 
-        return await queryWithAudit(sql, {
-            ...params,
-            logDescription: `Update: Printer Template(${name}) updated`
-        });
+        return await query(sql);
     },
 
     delete: async (params) => {
@@ -189,8 +290,8 @@ module.exports = {
             name
         } = params;
 
-        const sql = SQL`DELETE FROM 
-                            billing.printer_templates 
+        const sql = SQL`DELETE FROM
+                            billing.printer_templates
                         WHERE id = ${id}
                         RETURNING *, '{}'::jsonb old_values
                         `;
