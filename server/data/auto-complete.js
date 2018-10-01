a75012b5045e65a2facc3ee3d87a19e9fd8fb2ed
Kamran Payne <kpayne@viztek.net>
2018-10-01 10:36:58 -0400

    EXA-8952 fix sql error with dates

--- server/data/auto-complete.js
+++ server/data/auto-complete.js
@@ -179,7 +179,7 @@ module.exports = {
                     WHEN trim('${params.q}') ~* '\\d\\d/\\d\\d/\\d\\d\\d\\d'
                         THEN patients.birth_date = to_date(trim('${params.q}'), 'mm/dd/yyyy')
                     WHEN is_date('${params.q}') IS TRUE
-                        THEN patients.birth_date = '${params.q}'
+                        THEN patients.birth_date = to_date(trim('${params.q}'), 'yyyy-mm-dd')
                     ELSE
                         (patients.full_name ~* trim('${params.q}') OR patients.account_no ~* trim('${params.q}'))
                 END
