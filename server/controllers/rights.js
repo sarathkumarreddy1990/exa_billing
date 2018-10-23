030913a475bef2e4d423d239453ff82751fbea24
abirami <abirami@emdsys.com>
2018-10-23 16:29:42 +0530

    EXA -11167 | Billing module filters: give user ability to set default filter

--- server/controllers/rights.js
+++ server/controllers/rights.js
@@ -5,7 +5,14 @@ const _ = require('lodash');
 module.exports = {
 
     checkRights: function (args) {
-        const defaultAPIs = ['/exa_modules/billing/app_settings', '/exa_modules/billing/user_settings', '/exa_modules/billing/setup/study_filters', '/exa_modules/billing/study_filters'];
+        const defaultAPIs = [
+            '/exa_modules/billing/app_settings'
+            , '/exa_modules/billing/user_settings'
+            , '/exa_modules/billing/setup/study_filters'
+            , '/exa_modules/billing/study_filters'
+            , '/exa_modules/billing/setup/study_filters/set_default_tab'
+        ];
+		
         let {
             screens,
             userType,
