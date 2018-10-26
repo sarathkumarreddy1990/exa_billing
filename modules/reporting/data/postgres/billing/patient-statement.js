9aab043a65ecc8c4f2aa7aee34610f28ec195b3d
Vengadesh <vengadesh@emdsys.com>
2018-10-26 15:20:33 +0530

    EXA-11419  | Patients Statements Excel and CSV export formatted with $ and ,

--- modules/reporting/data/postgres/billing/patient-statement.js
+++ modules/reporting/data/postgres/billing/patient-statement.js
@@ -124,10 +124,7 @@ WITH claim_data AS (
           END) * (CASE WHEN type in ('payment','adjustment') then -1
                   ELSE 1
                   END)) AS amount,
-        case when type = 'charge' then amount end as charge,
-        case when type = 'payment' then amount end as payment,
-        case when type = 'adjustment' then amount end as adjustment,
-
+        <%= claimAmount %>
         <% if (payToProvider == true) {%>
           bp.name as billing_provider_name,
           bp.pay_to_address_line1 as billing_proaddress1,
@@ -207,8 +204,8 @@ WITH claim_data AS (
     SELECT
         dc.pid,
         dc.enc_id,
-        coalesce(payment_type_date1, payment_type_date2) AS bucket_date,
-        sum(dc.amount) AS enc_total_amount
+        COALESCE (payment_type_date1, payment_type_date2) AS bucket_date,
+        <%= encounterTotal %> AS enc_total_amount
     FROM detail_cte dc
     INNER JOIN date_cte dtc ON  dtc.pid = dc.pid
     GROUP BY
@@ -229,30 +226,24 @@ WITH claim_data AS (
     FROM sum_encounter_cte
     GROUP BY pid
     ),
-
     statement_cte AS (
-    SELECT
-        coalesce(statement_total_amount, 0::money) AS statement_total_amount,
-        coalesce(current_amount,0::money) AS current_amount,
-        coalesce(over30_amount,0::money) AS over30_amount,
-        coalesce(over60_amount,0::money) AS over60_amount,
-        coalesce(over90_amount,0::money) AS over90_amount,
-        coalesce(over120_amount,0::money) AS over120_amount,
-        (SELECT description
-         FROM billing.messages
-         WHERE company_id = 1
-         AND CODE = (SELECT CASE
-                     WHEN over120_amount IS NOT NULL THEN '>120'
-                     WHEN over90_amount IS NOT NULL THEN '91-120'
-                     WHEN over60_amount IS NOT NULL THEN '61-90'
-                     WHEN over30_amount IS NOT NULL THEN '31-60'
-                     WHEN current_amount IS NOT NULL THEN '0-30'
-                     ELSE 'collections'
-                     END)
-        ) AS billing_msg,
-        pid AS pid
-    FROM sum_statement_credit_cte
-    ),
+        SELECT
+            <%= statmentAging %>
+            (SELECT description
+             FROM billing.messages
+             WHERE company_id = 1
+             AND CODE = (SELECT CASE
+                         WHEN over120_amount IS NOT NULL THEN '>120'
+                         WHEN over90_amount IS NOT NULL THEN '91-120'
+                         WHEN over60_amount IS NOT NULL THEN '61-90'
+                         WHEN over30_amount IS NOT NULL THEN '31-60'
+                         WHEN current_amount IS NOT NULL THEN '0-30'
+                         ELSE 'collections'
+                         END)
+            ) AS billing_msg,
+            pid AS pid
+        FROM sum_statement_credit_cte
+        ),
 
     all_cte AS (
     -- 1st Header, Billing Provider, update the columns in the dataset
@@ -475,32 +466,17 @@ WITH claim_data AS (
     , c12
     , c13
     , c14
-    <% if (reportFormat == 'xlsx' || reportFormat == 'csv' ) { %>
-        -- Query for XLSX and CSC format remove special characters like ($  and ,) from the amount column
-         , regexp_replace(c15, '[$|,]', '', 'g') AS c15
-         , regexp_replace(c16, '[$|,]', '', 'g') AS c16
-         , regexp_replace(c17, '[$|,]', '', 'g') AS c17
-         , regexp_replace(c18, '[$|,]', '', 'g') AS c18
-         , regexp_replace(c19, '[$|,]', '', 'g') AS c19
-         , regexp_replace(c20, '[$|,]', '', 'g') AS c20
-         , regexp_replace(c21, '[$|,]', '', 'g') AS c21
-         , regexp_replace(c22, '[$|,]', '', 'g') AS c22
-         , regexp_replace(c23, '[$|,]', '', 'g') AS c23
-         , regexp_replace(c24, '[$|,]', '', 'g') AS c24
-         , regexp_replace(c25, '[$|,]', '', 'g') AS c25
-       <% } else { %>
-         , c15
-         , c16
-         , c17
-         , c18
-         , c19
-         , c20
-         , c21
-         , c22
-         , c23
-         , c24
-         , c25
-       <% } %>
+    , c15
+    , c16
+    , c17
+    , c18
+    , c19
+    , c20
+    , c21
+    , c22
+    , c23
+    , c24
+    , c25
     , c26
     <%= rowFlag %>
     <%= encounterAmount %>
@@ -858,6 +834,22 @@ const api = {
             ELSE null
             END
             `;
+            filters.statmentAging  = `
+                COALESCE(statement_total_amount, 0::money) AS statement_total_amount,
+                COALESCE(current_amount,0::money) AS current_amount,
+                COALESCE(over30_amount,0::money) AS over30_amount,
+                COALESCE(over60_amount,0::money) AS over60_amount,
+                COALESCE(over90_amount,0::money) AS over90_amount,
+                COALESCE(over120_amount,0::money) AS over120_amount,
+           `;
+           filters.claimAmount = `
+                CASE WHEN type = 'charge' THEN amount end AS charge,
+                CASE WHEN type = 'payment' THEN amount end AS payment,
+                CASE WHEN type = 'adjustment' THEN amount end AS adjustment,
+            `;
+            filters.encounterTotal = `
+                SUM(dc.amount)
+            `;
         }
         else {
             filters.rowFlag = ``;
@@ -869,6 +861,22 @@ const api = {
             filters.billingMsg = ``;
             filters.CR = `|| ' CR'`;
             filters.anythingElse = `, null`;
+            filters.statmentAging = `
+                    COALESCE(statement_total_amount::NUMERIC, 0) AS statement_total_amount,
+                    COALESCE(current_amount::NUMERIC,0) AS current_amount,
+                    COALESCE(over30_amount::NUMERIC,0) AS over30_amount,
+                    COALESCE(over60_amount::NUMERIC,0) AS over60_amount,
+                    COALESCE(over90_amount::NUMERIC,0) AS over90_amount,
+                    COALESCE(over120_amount::NUMERIC,0) AS over120_amount,
+            `;
+            filters.claimAmount = `
+                CASE WHEN type = 'charge' THEN amount::NUMERIC END AS charge,
+                CASE WHEN type = 'payment' THEN amount::NUMERIC END AS payment,
+                CASE WHEN type = 'adjustment' THEN amount::NUMERIC END AS adjustment,
+            `;
+             filters.encounterTotal = `
+                SUM(dc.amount::NUMERIC)
+            `;
         }
 
         return {
