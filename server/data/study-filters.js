4ed09a966e336dc76041e5537b4a58259caf915f
Vengadesh <vengadesh@emdsys.com>
2019-01-24 17:21:21 +0530

    EXA-12195 | When a claim global filter is created by billing user then is not holding the conditions for other user

--- server/data/study-filters.js
+++ server/data/study-filters.js
@@ -1,34 +1,35 @@
-const { query } = require('./index');
-
-
+const {
+    SQL,
+    query
+} = require('./index');
 
 module.exports = {
-
     getData: async function (args) {
+        const sqlQuery = SQL `
+                    SELECT
+                        id AS filter_id, *
+                    FROM
+                        billing.grid_filters
+                    WHERE
+                        filter_type = 'studies'
+                        AND (user_id = ${args.userId} OR is_global_filter)
+                        AND inactivated_dt is null
+                    ORDER BY
+                         filter_order `;
 
-        const sqlQuery = `
-                    SELECT id AS filter_id, *
-                    FROM   billing.grid_filters
-                    WHERE  filter_type = 'studies'
-                            AND (user_id = $1 OR is_global_filter)
-                            AND inactivated_dt is null
-                    ORDER  BY filter_order
-                        `;
-
-        return await query(sqlQuery, [args.userId]);
-
+        return await query(sqlQuery);
     },
 
     getUserWLFilters: async function (args) {
-
-        const sqlQuery = `
+        const sqlQuery = SQL `
                         SELECT
-                        filter_info    AS perm_filter,
-                        grid_filters.*
-                        FROM billing.grid_filters
-                        WHERE grid_filters.id = $1
-        `;
+                            filter_info  AS perm_filter,
+                            grid_filters.*
+                        FROM
+                            billing.grid_filters
+                        WHERE
+                            grid_filters.id = ${args.id} `;
 
-        return await query(sqlQuery, [args.id]);
+        return await query(sqlQuery);
     }
 };
