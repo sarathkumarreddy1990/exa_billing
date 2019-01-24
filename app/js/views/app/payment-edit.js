b2d327076c8a94cef646515fae5d2557cecdfeee
Senthilkumar <senthilkumar@emdsys.com>
2019-01-24 19:02:18 +0530

    EXA-12387 | Payment applied to claims means shouldn't allow to delete

--- app/js/views/app/payment-edit.js
+++ app/js/views/app/payment-edit.js
@@ -2837,7 +2837,7 @@ define(['jquery',
 
             afterAppliedGridBind: function (dataset, e, self) {
                 if (dataset && dataset.length > 0) {
-                    $('#selectPayerType').attr({ 'disabled': true, 'title': 'You cannot change the payer since the payment has already applied' })
+                    $('#selectPayerType').attr({ 'disabled': true, 'i18nt': 'billing.payments.youCannotChangeThePayerSinceThePaymentHasAlreadyApplied' })
                 }
             },
 
