751914071e27fd4ed208e906a5b43d13bb160f8c
RameshPrabhakar <ramesh.prabhakar@emdsys.com>
2019-01-29 21:03:54 +0530

    EXA-12410 | Report not displaying for the claim in right click view reports. - local commit 2

--- server/data/claim/claim-workbench.js
+++ server/data/claim/claim-workbench.js
@@ -361,16 +361,18 @@ module.exports = {
         } = params;
 
         let sql = SQL`
-                    SELECT  study_id,
-                            studies.order_id,
-                            studies.study_status,
-                            CASE
-	                            WHEN studies.study_status = 'APP' THEN 1
-	                            ELSE 2
-	                        END  AS status_index
-					FROM    billing.charges_studies
-                            INNER JOIN billing.charges ON billing.charges.id = billing.charges_studies.charge_id
-                            INNER JOIN public.studies ON public.studies.id = billing.charges_studies.study_id
+                    SELECT
+                        study_id,
+                        studies.order_id,
+                        studies.study_status,
+                        CASE
+                            WHEN studies.study_status = 'APP' THEN 1
+                            ELSE 2
+                        END  AS status_index
+                    FROM
+                        billing.charges_studies
+                    INNER JOIN billing.charges ON billing.charges.id = billing.charges_studies.charge_id
+                    INNER JOIN public.studies ON public.studies.id = billing.charges_studies.study_id
 					WHERE   billing.charges.claim_id = ${claim_id}
                     ORDER BY status_index ,study_id
                     LIMIT 1`;
