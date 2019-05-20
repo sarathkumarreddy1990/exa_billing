456c1fddc9b5f7b46ba0660ab96cde3878d94b9e
Vengadesh <vengadesh@emdsys.com>
2019-05-20 16:12:10 +0530

    EXA-15058 | Payment Report: Add column to show adjustment code

--- modules/reporting/data/postgres/billing/payment-report.js
+++ modules/reporting/data/postgres/billing/payment-report.js
@@ -138,14 +138,13 @@ const detailQueryTemplate = _.template(`
                 bc.id  claim_id,
                 SUM(CASE WHEN amount_type= 'payment' then bpa.amount  else 0::money end) AS applied_amount,
                 SUM(CASE WHEN amount_type= 'adjustment' then bpa.amount  else 0::money end) AS adjustment,
-                MAX(bac.description) AS description
+                bac.description AS description
                 <% if (userIds) { %> , MAX(users.username) AS user_name    <% } %>
             FROM
                 billing.payments bp
             LEFT JOIN billing.payment_applications bpa on bpa.payment_id = bp.id
             LEFT JOIN billing.charges bch on bch.id = bpa.charge_id
-            LEFT Join billing.claims  bc on bc.id = bch.claim_id
-            LEFT JOIN billing.adjustment_codes bac ON bac.id = bpa.adjustment_code_id
+            LEFT JOIN billing.claims  bc on bc.id = bch.claim_id
             <% if (billingProID) { %>  INNER JOIN billing.providers bpr ON bpr.id = bc.billing_provider_id <% } %>
             <% if (userIds) { %>  INNER join public.users  users on users.id = bp.created_by    <% } %>
             <% if (userRoleIds) { %>
@@ -170,13 +169,24 @@ const detailQueryTemplate = _.template(`
                   <% if (adjustmentCodeIds) { %> AND  <% print(adjustmentCodeIds); } %>
                 )have_adjustment on have_adjustment.payment_id = bp.id and have_adjustment.charge_id = bpa.charge_id
                <% } %>
+               LEFT JOIN LATERAL (
+                   SELECT
+                       i_bac.description
+                   FROM
+                       billing.payment_applications i_bpa
+                   INNER JOIN billing.adjustment_codes i_bac ON i_bac.id = i_bpa.adjustment_code_id
+                   WHERE
+                       i_bpa.payment_id = bp.id
+                   ORDER BY i_bpa.id
+                   LIMIT 1
+              ) bac ON TRUE
            WHERE
             <%= claimDate %>
             <% if (facilityIds) { %>AND <% print(facilityIds); } %>
             <% if(billingProID) { %> AND <% print(billingProID); } %>
             <% if (userIds) { %>AND <% print(userIds); } %>
             <% if (userRoleIds) { %>AND <% print(userRoleIds); } %>
-            GROUP BY bp.id,bc.id )
+            GROUP BY bp.id, bc.id, bac.description )
                 SELECT
                     to_char(p.accounting_date, '<%= dateFormat %>')   AS "Accounting Date",
                     f.facility_name  AS "Facility Name",
