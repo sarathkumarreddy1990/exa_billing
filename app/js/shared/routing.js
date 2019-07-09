58783e947a3a7d838a39c018ca4aa08c9b19d259
Vengadesh <vengadesh@emdsys.com>
2019-07-09 18:07:50 +0530

    EXA-12346 | Payments by Ins Company Report: add filter for Insurance provider

---
+++ app/js/shared/routing.js
@@ -0,0 +1,21 @@
+define([
+    'jquery'
+    , 'underscore'
+    , 'backbone'
+],
+    function ($, _, Backbone) {
+        var Routing = {
+            clearView: function (view) {
+                if (view) {
+                    var parent = view.$el.parent();
+                    view.undelegateEvents();
+                    view.stopListening();
+                    view.unbind();
+                    view.remove();
+                    view.$el.removeData().unbind();
+                    parent.append('<div id="data_container" class="page-full"></div>');
+                }
+            }
+        }
+        return Routing;
+    });
