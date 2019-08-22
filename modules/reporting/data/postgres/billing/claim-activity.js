9dafcb18a5d12da8de2e20a3b5ef155366c31513
Vengadesh <vengadesh@emdsys.com>
2019-08-22 12:39:20 +0530

    EXA-17013 | Claim date is showing less than a date in billing reports

--- modules/reporting/data/postgres/billing/claim-activity.js
+++ modules/reporting/data/postgres/billing/claim-activity.js
@@ -15,8 +15,8 @@ SELECT
     get_full_name(p.last_name,p.first_name,p.middle_name,p.prefix_name,p.suffix_name) AS patient_name,
     p.account_no AS account_no,
     timezone(get_facility_tz(bc.facility_id::integer), bc.claim_dt)::DATE AS claim_date,
-	get_full_name(ppref.last_name,ppref.first_name,ppref.middle_initial,null,ppref.suffix) AS referring_physician,
-	get_full_name(ppren.last_name,ppren.first_name,ppren.middle_initial,null,ppren.suffix) AS reading_physician,
+    get_full_name(ppref.last_name, ppref.first_name, ppref.middle_initial, null, ppref.suffix) AS referring_physician,
+    get_full_name(ppren.last_name, ppren.first_name, ppren.middle_initial, null, ppren.suffix) AS reading_physician,
     pg.group_name AS ordering_facility,
     f.time_zone AS facility_timezone,
     f.facility_code AS facility_code
