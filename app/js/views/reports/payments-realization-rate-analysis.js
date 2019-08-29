a781690af269b8f731e083ba00979274fb4af3c4
Vengadesh <vengadesh@emdsys.com>
2019-08-29 15:03:07 +0530

    EXA-17260 | Showing wrong result in payment realization rate analysis report

--- app/js/views/reports/payments-realization-rate-analysis.js
+++ app/js/views/reports/payments-realization-rate-analysis.js
@@ -124,8 +124,8 @@ define([
                     self.viewModel.accountingDateTo = end;
                 });
                 accountingDate.on('cancel.daterangepicker', function (ev, drp) {
-                    self.viewModel.serviceDateFrom = null;
-                    self.viewModel.serviceDateTo = null;
+                    self.viewModel.accountingDateFrom = null;
+                    self.viewModel.accountingDateTo = null;
                 });
                 commonjs.isMaskValidate();
             },
@@ -159,6 +159,14 @@ define([
             hasValidViewModel: function () {
                 if (this.viewModel.reportId == null || this.viewModel.reportCategory == null || this.viewModel.reportFormat == null) {
                     commonjs.showWarning('messages.status.pleaseCheckReportIdCategoryandorFormat');
+                    return;
+                }
+                if (!this.viewModel.accountingDateFrom || !this.viewModel.accountingDateTo) {
+                    commonjs.showWarning('messages.warning.payments.selectAccountingDate');
+                    return;
+                }
+                if (!this.viewModel.serviceDateFrom || !this.viewModel.serviceDateTo) {
+                    commonjs.showWarning('messages.status.pleaseSelectDateRange');
                     return;
                 }
                 return true;
