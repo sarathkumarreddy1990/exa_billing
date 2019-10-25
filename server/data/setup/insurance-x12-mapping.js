02e1368c05678e37b5c5011d0cde892331b55067
senthilkumar <senthilkumar@emdsys.com>
2019-10-25 16:48:27 +0530

    EXA-18742 |  AHS - disallow "electronic billing" method for insurances

--- server/data/setup/insurance-x12-mapping.js
+++ server/data/setup/insurance-x12-mapping.js
@@ -66,7 +66,8 @@ module.exports = {
 
         const sql = SQL`SELECT
                               ip.id
-                            , ip.insurance_name
+                            , ip.insurance_name,
+                            , ip.insurance_code
                             , ch.id AS claimclearinghouse
                             , billing_method
                             , claim_filing_indicator_code AS indicator_code
