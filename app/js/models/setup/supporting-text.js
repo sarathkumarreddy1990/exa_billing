e510067bb790b4f13436cfac042a91c4c0b82fc5
Matthew Farmer <mfarmer@kmha.com>
2020-01-15 22:47:04 -0500

    EXA-17185 | Removed irrelevant items from backbone model

--- app/js/models/setup/supporting-text.js
+++ app/js/models/setup/supporting-text.js
@@ -2,10 +2,7 @@ define(['backbone'], function (Backbone) {
     var supportingTextModel = Backbone.Model.extend({
         urlRoot: "/exa_modules/billing/setup/supporting_text",
         defaults: {
-            companyId: "",
-            processType: "",
-            processStatus: "",
-            colorCode: ""
+            companyId: ""
         },
         initialize: function (models) {
         }
