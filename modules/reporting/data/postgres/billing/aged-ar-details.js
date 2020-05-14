39f59b35c6f1c5ea04a6f383ef403ef2bfdb2c20
Vengadesh <vengadesh@emdsys.com>
2020-05-14 17:23:06 +0530

    EXA-22100 | Aged AR summary and details report

--- modules/reporting/data/postgres/billing/aged-ar-details.js
+++ modules/reporting/data/postgres/billing/aged-ar-details.js
@@ -155,8 +155,8 @@ LEFT JOIN public.insurance_providers pip ON pip.id = ppi.insurance_provider_id
  LEFT JOIN public.provider_contacts ppc ON ppc.id = bc.referring_provider_contact_id
  LEFT JOIN public.providers ppr ON ppr.id = ppc.provider_id
   <% if (billingProID) { %> INNER JOIN billing.providers bp ON bp.id = bc.billing_provider_id <% } %>
-      WHERE TRUE
-      AND <%=companyId%>
+      WHERE
+      <%=companyId%>
       <% if(incPatDetail == 'true') { %>
         AND payer_type != 'patient'
       <%}%>
