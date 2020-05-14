497dfc55134a22ffc3332bc696a52a07698173ee
Vengadesh <vengadesh@emdsys.com>
2020-05-14 14:11:04 +0530

    EXA-22100 | Aged AR Report grand total wronly bind

--- modules/reporting/data/postgres/billing/aged-ar-details.js
+++ modules/reporting/data/postgres/billing/aged-ar-details.js
@@ -155,8 +155,11 @@ LEFT JOIN public.insurance_providers pip ON pip.id = ppi.insurance_provider_id
  LEFT JOIN public.provider_contacts ppc ON ppc.id = bc.referring_provider_contact_id
  LEFT JOIN public.providers ppr ON ppr.id = ppc.provider_id
   <% if (billingProID) { %> INNER JOIN billing.providers bp ON bp.id = bc.billing_provider_id <% } %>
-      WHERE 1 = 1
+      WHERE TRUE
       AND <%=companyId%>
+      <% if(incPatDetail == 'true') { %>
+        AND payer_type != 'patient'
+      <%}%>
       <% if (facilityIds) { %>AND <% print(facilityIds); } %>
       <% if(billingProID) { %> AND <% print(billingProID); } %>
       <% if(excCreditBal == 'true'){ %> AND  gcd.balance::money > '0' <% } %>
@@ -293,7 +296,7 @@ SELECT
 	  "Total"
 FROM
     aging_result
-ORDER BY   "Responsible Party_order_by","Payer Name", "Responsible Party"
+ORDER BY  "Responsible Party_order_by","Payer Name", "Responsible Party"
 
 
 `);
