624b5c9b2016f28981e040bf2e1915e9928f7d19
R. RAMESH PRABHAKAR <ramesh.prabhakar@emdsys.com>
2020-10-19 10:04:47 +0530

    EXA-15189 | Collection process - query written
    (cherry picked from commit c8e283cfdccbb9987104531820e9e4121e43a31f)

--- server/data/setup/collection-process.js
+++ server/data/setup/collection-process.js
@@ -126,7 +126,7 @@ const acr = {
             ip,
             companyId,
             screenName = 'AutoCollectionReview',
-            moduleName = 'Payments',
+            moduleName = 'payments',
         } = params;
 
         let auditDetails = {
@@ -146,6 +146,13 @@ const acr = {
             };
         }
 
+        if (companySettings && !companySettings.rowCount) {
+            return {
+                status: 'Not Configured',
+                error: null
+            };
+        }
+
         let {
             acr_claim_status_id = null
             , acr_min_balance_amount = null
@@ -155,11 +162,14 @@ const acr = {
             , acr_claim_status_last_payment_days = null
         } = companySettings.rowCount && companySettings.rows[0] || {};
 
-        let writeOffQuery ='';
-        let selectResult = 'SELECT id, null, null FROM update_status';
+        let writeOffQuery = '';
+        let selectResult = SQL`
+          SELECT null, null, id::text FROM update_status
+           UNION ALL
+          SELECT null, null, id::text FROM insert_claim_comment_audit_cte `;
 
         if (acr_write_off_adjustment_code_id) {
-            writeOffQuery= SQL`
+            writeOffQuery = SQL`
                 -- --------------------------------------------------------------------------------------------------------------
                 -- Create payments for write-off adjustment
                 -- --------------------------------------------------------------------------------------------------------------
@@ -259,7 +269,7 @@ const acr = {
                         billing.claims
                     INNER JOIN insert_payment ip ON ip.patient_id = claims.patient_id
                     INNER JOIN billing.claim_status cs ON cs.id = claims.claim_status_id
-                    INNER JOIN claim_payments_list cpl ON cpl.claim_id = claims.id
+                    INNER JOIN claim_payment_lists cpl ON cpl.claim_id = claims.id
                     INNER JOIN billing.charges bch ON bch.claim_id = claims.id
                     INNER JOIN public.cpt_codes pcc on pcc.id = bch.cpt_id
                     INNER JOIN LATERAL billing.get_charge_other_payment_adjustment(bch.id) bgct ON TRUE
@@ -348,11 +358,15 @@ const acr = {
                     GROUP BY claim_charges.claim_id
                 )`;
 
-            selectResult = `
+            selectResult.append(`
+                 UNION ALL
                 SELECT details,null,null FROM credit_adjustment_charges
-		        UNION ALL
-		        SELECT details,null,null FROM debit_adjustment_charges `;
+                 UNION ALL
+                SELECT details,null,null FROM debit_adjustment_charges
+                 UNION ALL
+                SELECT null,null,result::text FROM change_responsible_party `);
         }
+
 
         const sql = SQL`WITH
                 -- --------------------------------------------------------------------------------------------------------------
@@ -376,19 +390,57 @@ const acr = {
                     ) AS last_patient_statement ON TRUE
                     LEFT JOIN LATERAL (
                         SELECT
-                            MAX(applied_dt) AS last_payment_dt
+                            timezone(get_facility_tz(c.facility_id::integer), MAX(applied_dt))::DATE AS last_payment_dt
                         FROM billing.charges AS ch
                         INNER JOIN billing.payment_applications AS pa ON pa.charge_id = ch.id
                         WHERE ch.claim_id = c.id
                         AND pa.amount != 0::money
                     ) AS payment_details ON TRUE
-                    WHERE
-                    CASE
-                        WHEN ( payment_details.last_payment_dt::DATE - last_patient_statement.created_dt ) > ${acr_claim_status_statement_days} THEN TRUE
-                        WHEN ( timezone(get_facility_tz(c.facility_id::integer), now()::timestamp)::DATE - payment_details.last_payment_dt::DATE  ) > ${acr_claim_status_last_payment_days} THEN TRUE
-                    ELSE FALSE
+                    WHERE last_patient_statement.created_dt IS NOT NULL `;
+
+        if (acr_claim_status_statement_count && acr_claim_status_statement_days && acr_claim_status_last_payment_days) {
+            sql.append(`AND
+                NOT ( CASE
+                        WHEN  payment_details.last_payment_dt IS NULL AND last_patient_statement.created_dt IS NOT NULL THEN FALSE
+                        WHEN  payment_details.last_payment_dt IS NOT NULL AND last_patient_statement.created_dt IS NOT NULL THEN
+                            CASE
+                                WHEN (payment_details.last_payment_dt
+                                    BETWEEN (last_patient_statement.created_dt) AND (last_patient_statement.created_dt + interval '${acr_claim_status_statement_days} days')::DATE)  THEN TRUE
+                                WHEN (payment_details.last_payment_dt
+                                    BETWEEN (payment_details.last_payment_dt + interval '1 days') AND (payment_details.last_payment_dt + interval '${acr_claim_status_last_payment_days} days')::DATE)
+                                    THEN TRUE
+                                ELSE FALSE
+                            END
                     END
-                    ORDER BY p.id ASC
+                )`);
+        } else if (acr_claim_status_statement_count && acr_claim_status_statement_days && !acr_claim_status_last_payment_days) {
+            sql.append(`AND
+                NOT ( CASE
+                        WHEN  payment_details.last_payment_dt IS NULL AND last_patient_statement.created_dt IS NOT NULL THEN FALSE
+                        WHEN  payment_details.last_payment_dt IS NOT NULL AND last_patient_statement.created_dt IS NOT NULL THEN
+                            CASE
+                                WHEN (payment_details.last_payment_dt
+                                    BETWEEN (last_patient_statement.created_dt) AND (last_patient_statement.created_dt + interval '${acr_claim_status_statement_days} days')::DATE)  THEN TRUE
+                                ELSE FALSE
+                            END
+                    END
+                )`);
+        } else {
+            sql.append(`AND
+                NOT ( CASE
+                        WHEN  payment_details.last_payment_dt IS NULL AND last_patient_statement.created_dt IS NOT NULL THEN FALSE
+                        WHEN  payment_details.last_payment_dt IS NOT NULL AND last_patient_statement.created_dt IS NOT NULL THEN
+                            CASE
+                                WHEN (payment_details.last_payment_dt
+                                    BETWEEN (payment_details.last_payment_dt + interval '1 days') AND (payment_details.last_payment_dt + interval '${acr_claim_status_last_payment_days} days')::DATE)
+                                    THEN TRUE
+                                ELSE FALSE
+                            END
+                    END
+                )`);
+        }
+
+        sql.append(` ORDER BY p.id ASC
                 )
                 , claim_charge_fee AS (
                     SELECT
@@ -439,7 +491,7 @@ const acr = {
                         ,p.id AS patient_id
                         ,p.full_name
                         ,p.facility_id
-                        ,ARRAY_AGG(claims.id) AS claim_ids
+                        ,ARRAY_AGG(claims.id) FILTER ( WHERE claim_status_id != ${acr_claim_status_id} ) AS claim_ids
                         ,ARRAY_AGG(claims.id) FILTER ( WHERE claim_status_id = (SELECT id FROM billing.claim_status WHERE code = 'CIC')) AS collection_claim_ids
                     FROM
                         billing.claims
@@ -458,16 +510,51 @@ const acr = {
                         SET claim_status_id = ${acr_claim_status_id}
                     FROM claim_patient_balance
                     WHERE id = ANY(claim_ids)
-                    AND claim_status_id != (SELECT id FROM billing.claim_status WHERE code = 'CIC')
+                    AND NOT (claim_status_id = ANY(ARRAY[(SELECT id FROM billing.claim_status WHERE code = 'CIC'), ${acr_claim_status_id}]))
                     RETURNING id
                 )
-               `;
+                , insert_claim_comments AS (
+                    INSERT INTO billing.claim_comments (
+                        note
+                        , type
+                        , claim_id
+                        , created_by
+                        , created_dt
+                    )
+                    SELECT
+                       'Claim was sent to collections review'
+                       , 'auto'
+                       , UNNEST(claim_ids)
+                       , ${userId}
+                       , now()
+                    FROM claim_patient_balance
+                    RETURNING *, '{}'::jsonb old_values
+               )
+               , insert_claim_comment_audit_cte as(
+                    SELECT billing.create_audit(
+                        ${auditDetails.company_id}
+                        , '${auditDetails.screen_name}'
+                        , id
+                        , '${auditDetails.screen_name}'
+                        , '${auditDetails.module_name}'
+                        , 'Claim Comments inserted AS Claim was sent to collections review'
+                        , '${auditDetails.client_ip}'
+                        , json_build_object(
+                            'old_values', COALESCE(old_values, '{}'),
+                            'new_values', ('{}')::text
+                        )::jsonb
+                        , ${userId}
+                    ) AS id
+                    FROM insert_claim_comments
+                    WHERE id IS NOT NULL
+                )
+               `);
 
         sql.append(writeOffQuery);
 
         sql.append(selectResult);
 
-        return await query(sql);
+        return await query(sql.text, sql.values);
     }
 };
 
