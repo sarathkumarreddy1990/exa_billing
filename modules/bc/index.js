7d4c9b21bc48c3811d9e9484fa54e84f8d7fcac7
senthilkumara <senthilkumar@emdsys.com>
2020-10-29 00:45:04 +0530

    EXA-23971 | Cron service billing changes for downloading and process MSP payment files

--- modules/bc/index.js
+++ modules/bc/index.js
@@ -7,9 +7,22 @@ const logger = require('../../logger');
 const moment = require('moment');
 const crypto = require('crypto');
 const fse = require('fs-extra');
+const mkdirp = require('mkdirp');
+const path = require('path');
 const {
     promisify,
 } = require('util');
+const request = require('request-promise-native');
+const downtime = require('../bc/resx/downtime.json');
+const siteConfig = require('../../server/config');
+const externalUrlBc = siteConfig.get('externalUrlBc');
+const externalUrlBcUserName = siteConfig.get('externalUrlBcUserName');
+const externalUrlBcPassword = siteConfig.get('externalUrlBcPassword');
+const parser = require('./decoder');
+const bcData = require('../../server/data/bc');
+const eraData = require('../../server/data/era');
+const writeFileAsync = promisify(fs.writeFile);
+const mkdirpAsync = promisify(mkdirp);
 const statAsync = promisify(fs.stat);
 
 const bcModules = {
@@ -66,12 +79,142 @@ const bcModules = {
     },
 
     /**
+     * isDownTime - to check down time of MSP
+     *
+     * @param  {String} startTime  Start down time
+     * @param  {String} endTime  End down time
+     * @param  {String} tmz  Time zone
+     */
+    isDownTime: (startTime, endTime, tmz) => {
+
+        let format  = 'HH:mm';
+        let curTime = moment().tz(tmz).format(format);
+        let time = moment(curTime, format, tmz);
+        let beforeTime = moment(startTime, format, tmz);
+        let afterTime = moment(endTime, format, tmz);
+
+        return time.isBetween(beforeTime, afterTime);
+    },
+
+
+    /**
+     * doRequest - Request third part application
+     *
+     * @param  {String} args
+     * @param {String} time zone
+     */
+    doRequest: async (requestOptions, tmz) => {
+        let weekDay = moment.tz(tmz).day();
+        let downtimeWeekDay = downtime[weekDay];
+        let isDownTime = false;
+
+        if(!externalUrlBcUserName || !externalUrlBcPassword || !externalUrlBc){
+            logger.error('MSP Username/Password/URL missing in web config');
+            return { error: 'webConfigError' };
+        }
+
+        for (let i = 0; i < downtimeWeekDay.length; i++) {
+            let value = downtimeWeekDay[i];
+
+            if (bcModules.isDownTime(value.startTime, value.endTime, tmz)) {
+                isDownTime = true;
+                break;
+            }
+        }
+
+        if (isDownTime) {
+            return { error: 'isDownTime' };
+        }
+
+        try {
+
+            let enableSessionOptions = {
+                method: 'POST',
+                uri: externalUrlBc,
+                rejectUnauthorized: false,
+                resolveWithFullResponse: true,
+                form: {
+                    ExternalAction: 'AsignOn'
+                }
+            };
+
+            //MSP Teleplan Web Service sign in
+
+            enableSessionOptions.form = {
+                ...enableSessionOptions.form,
+                username: externalUrlBcUserName,
+                password: externalUrlBcPassword
+            };
+
+            let enableSessionResponse = await request(enableSessionOptions);
+            let sessionResponse = await bcModules.convertToJson(enableSessionResponse.body);
+
+            if (sessionResponse.Result == 'SUCCESS') {
+                let cookieMSP = enableSessionResponse.headers['set-cookie'];
+                //MSP Teleplan Web Service accessing
+                let webServiceResponse = await request({
+                    ...requestOptions,
+                    headers: { Cookie: cookieMSP }
+                });
+
+                let disableSessionOptions = {
+                    method: 'POST',
+                    uri: externalUrlBc,
+                    rejectUnauthorized: false,
+                    resolveWithFullResponse: true,
+                    form: {
+                        ExternalAction: 'AsignOff'
+                    },
+                    headers: { Cookie: enableSessionResponse.headers['set-cookie'] }
+                };
+
+                //MSP Teleplan Web Service sign off
+                let disableResponse = await request(disableSessionOptions);
+                disableResponse = bcModules.convertToJson(disableResponse.body);
+
+                if (disableResponse.Result == 'SUCCESS') {
+                    return { data: webServiceResponse };
+                }
+
+                return { error: 'apiFailed' };
+            }
+
+            return { error: 'apiFailed' };
+        } catch (err) {
+            return { error: 'exceptionErrors' };
+        }
+    },
+
+    /**
      * convertToJson - Converting response from thirparty to json
      *
      * @param  {String} args
      * @param {String} time zone
      */
-    writeToFile: async (args, companyFileStoreDetails, encoderResult) => {
+    convertToJson(args) {
+        try {
+            let data = args.split(';');
+            let response = {};
+
+            data.forEach((value) => {
+                let result = value.split('=');
+                response[result[0]] = result[1];
+            });
+
+            return response;
+        } catch (err) {
+            logger.error('Error in processing for the response', err);
+            return { error: 'exceptionErrors' };
+        }
+    },
+
+    /**
+     * convertToJson - Converting response from thirparty to json
+     *
+     * @param  {String} args
+     * @param {String} time zone
+     */
+    writeToFile: async(args, companyFileStoreDetails, encoderResult) => {
         let {
             submittedClaim = [],
             totalClaimIdsSubmitted = [],
@@ -167,6 +310,195 @@ const bcModules = {
         }
 
     },
+
+    /*
+    * Used to throw custom error
+    * {@param} Error Message
+    */
+    sendDataError: (messge) => {
+        logger.error(messge);
+        throw new Error(messge);
+    },
+
+    /**
+    * Download the files from MSP portal
+    * {@param} params - which contains fileName, companyId of uploading file
+    */
+
+    downloadFiles: async (params) => {
+        let {
+            companyId,
+        } = params;
+
+        let fileStoreDetails = await bcController.getCompanyFileStore(companyId);
+        let {
+            file_store_id,
+            root_directory,
+        } = fileStoreDetails[0];
+
+        if (!root_directory) {
+            bcModules.sendDataError(`Company file store missing for companyId ${companyId}`);
+        }
+
+        try {
+            await statAsync(root_directory);
+        }
+        catch (e) {
+            bcModules.sendDataError(`Company file store folder missing in server file system ${e}`);
+        }
+
+        let downloadParams = {
+            uri: externalUrlBc,
+            method: 'POST',
+            rejectUnauthorized: false,
+            form: {
+                ExternalAction: 'AgetRemit',
+                remittance: true,
+                retrieve: 'Retrieve'
+            }
+        };
+
+        // Connect MSP Portal and Download Remittance File.
+        logger.info('Establishing connectivity with MSP Portal');
+        const {
+            data = null,
+            error = null
+        } = await bcModules.doRequest(downloadParams);
+
+        //Error Validations in MSP portal connectivity
+        if (error) {
+            logger.error(`MSP Portal Response Error: ${error}`);
+
+            return {
+                err: error
+            };
+        }
+
+        if (data) {
+            let mspFileResponse = data.split('#');
+
+            if (mspFileResponse && mspFileResponse.length) {
+                let fileContent = mspFileResponse[0] || '';
+                let fileProperties = bcModules.convertToJson(mspFileResponse[1]);
+
+                if (fileProperties.Result !== 'SUCCESS') {
+                    logger.error('No file downloaded from MSP Portal');
+
+                    return {
+                        err: null,
+                        message: 'No file downloaded from msp portal'
+                    };
+                }
+
+                // Reading the file content after gets downloaded
+                const bufferString = fileContent && fileContent.trim() || '';
+                const fileName = fileProperties.Filename;
+
+                //check for valid file content
+                let isInValidFileContent = bufferString.indexOf('M01') == -1 || bufferString.indexOf('VTC') == -1;
+                let fileBuffer = bufferString.split('/n');
+                let isEmptyRemittance = !isInValidFileContent && fileBuffer.length <= 2;
+
+                if (isInValidFileContent) {
+                    logger.error(`Invalid Remittance File ${fileName}`);
+
+                    return {
+                        status: 'INVALID_FILE'
+                    };
+                }
+
+                let fileMd5 = crypto.createHash('MD5').update(bufferString, 'utf8').digest('hex');
+
+                let {
+                    rows = []
+                } = await eraData.isProcessed(fileMd5, 1);
+
+                const {
+                    file_store_info = [],
+                    file_exists = []
+                } = rows.length && rows[0];
+
+                if (!file_store_info.length) {
+                    return {
+                        file_store_status: 'FILE_STORE_NOT_EXISTS'
+                    };
+                }
+
+                const fileStorePath = file_store_info[0].root_directory;
+                const fileStoreId = file_store_info[0].file_store_id;
+                const fileExist = file_exists.length && file_exists[0];
+                const created_dt = moment().format('YYYY/MM/DD');
+                const fileDir = `MSP/Remittance/${created_dt}`;
+                let filePath = `${root_directory}/${fileDir}`;
+
+                await mkdirpAsync(filePath);
+
+                if (fileExist) {
+                    logger.info(`Duplicate Remittance file: ${fileMd5}`);
+
+                    return {
+                        status: 'DUPLICATE_FILE',
+                        duplicate_file: true
+                    };
+                }
+
+                logger.info(`Writing file in Disk - ${filePath}/${fileName}`);
+                try {
+                    await writeFileAsync(`${filePath}/${fileName}`, bufferString, 'utf8');
+                } catch (err) {
+                    logger.error(`Error occurred on writing file into disk ${err}`);
+                    return err;
+                }
+
+                const statAfter = await statAsync(`${filePath}/${fileName}`);
+                let fileSize = statAfter.size;
+
+                logger.info('Writing file in DB');
+
+                try {
+                    await bcController.storeFile({
+                        file_name: fileName,
+                        file_store_id: fileStoreId,
+                        file_path: fileDir,
+                        file_md5: fileMd5,
+                        file_size: fileSize,
+                        companyId,
+                        file_type: 'can_bc_remit'
+                    });
+
+                } catch (err) {
+                    logger.error(`Error occured on writing file into db: ${err}`);
+                    return err;
+                }
+
+                logger.info(`Remittance File ${fileName} downloaded successfully`);
+                return {
+                    status: `ok`,
+                    message: `Remittance File ${fileName} downloaded successfully`
+                };
+            }
+        }
+    },
+
+    /**
+    * To process the Batch Eligibility Response from remittance file
+    */
+    processEligibilityResponse: async (response, params) => {
+        const eligibilityDetails = response && response.batchEligibilityResponse || [];
+
+        try {
+            if (!eligibilityDetails.length) {
+                logger.logInfo('No Eligibility Response found');
+                return;
+            }
+
+            return await bcData.storeEligibilityResponse(eligibilityDetails, params);
+        }
+        catch (err) {
+            logger.error('Error Occured in Batch Eligibility Response Processing');
+            return err;
+        }
+    }
 
 };
 
--- server/routes/cron-handlers.js
+++ server/routes/cron-handlers.js
@@ -7,6 +7,7 @@ const logger = require('../../logger');
 const ohip = require('../../modules/ohip');
 const ahs = require('../../modules/ahs/sftp');
 const httpHandler = require('../shared/http');
+const bc = require('../../modules/bc/cron');
 
 const restrictAccess = ( req, res, next ) => {
     const ipList = [
@@ -57,6 +58,7 @@ const runJob = ( req, res ) => {
                 'inProgress': false,
             });
         }
+
         return res.send({
             'successful': true,
             'inProgress': false,
@@ -69,28 +71,38 @@ router.get('/ohip/:endpoint', restrictAccess, checkProgress, runJob);
 const handleEvents = async (req, res) => {
     let {
         ip,
-        session,
+        session: {
+            company_id
+        } = {},
         params,
         query,
     } = req;
 
-    let company_id = 0;
-    if ( session && session.company_id > 0 ) {
-        company_id = session.company_id;
-    }
-    else if ( query && query.company_id > 0 ) {
-        company_id = query.company_id;
+    let companyId = 0;
+    let response;
+
+    companyId = company_id || query && query.company_id;
+
+    if (params.province === 'bc') {
+        response = await bc.events({
+            ...params,
+            ...query,
+            companyId,
+            ip,
+            isCron: true
+        });
+    } else if (params.province === 'ahs') {
+        response = await ahs.events({
+            ...params,
+            ...query,
+            company_id,
+            ip,
+        });
     }
 
-    let response = await ahs.events({
-        ...params,
-        ...query,
-        company_id,
-        ip,
-    });
     return httpHandler.send(req, res, response);
 };
 
-router.get('/ahs/files/:action', restrictAccess, handleEvents);
+router.get('/:province/files/:action', restrictAccess, handleEvents);
 
 module.exports = router;
