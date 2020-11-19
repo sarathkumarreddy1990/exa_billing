ff442558b28b898a2caef130becf9d1d26356802
senthilkumara <senthilkumar@emdsys.com>
2020-11-19 20:41:40 +0530

    EXA-24987 | Hided Canada related columns for US configuration

--- app/js/views/user-settings.js
+++ app/js/views/user-settings.js
@@ -214,9 +214,23 @@ define([
                         } else {
                             self.billingDisplayFields = _.reject(self.billingDisplayFields, function (field) { return (field && field.field_code == "payment_id") }) || [];
                         }
+
                         if (app.billingRegionCode !== "can_MB") {
-                            self.billingDisplayFields = _.reject(self.billingDisplayFields, function (field) { return (field && field.field_code == "can_mhs_microfilm_no") }) || [];
+                            self.billingDisplayFields = _.reject(self.billingDisplayFields, function (field) { return (field && (field.field_code == "can_mhs_microfilm_no")) }) || [];
                         }
+
+                        if (app.billingRegionCode !== "can_AB") {
+                            self.billingDisplayFields = _.reject(self.billingDisplayFields, function (field) { return (field && (field.field_code == "can_ahs_claim_no" || field.field_code == "claim_action")) }) || [];
+                        }
+
+                        if (app.billingRegionCode !== "can_BC") {
+                            self.billingDisplayFields = _.reject(self.billingDisplayFields, function (field) { return (field && (field.field_code == "can_bc_claim_sequence_numbers")) }) || [];
+                        }
+
+                        if (['can_AB', 'can_BC', 'can_MB'].indexOf(app.billingRegionCode) == -1) {
+                            self.billingDisplayFields = _.reject(self.billingDisplayFields, function (field) { return (field && (field.field_code == "phn_alt_account")) }) || [];
+                        }
+
                         var result_data = data && data.length && data[1] && data[1].rows && data[1].rows.length ? data[1].rows[0] : {};
                         self.checkedBillingDisplayFields = result_data.field_order || [] ;
                         self.checkedFields = self.checkedBillingDisplayFields ? self.checkedBillingDisplayFields : [];
