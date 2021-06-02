8732b3856154392db6a07c39547ff30bb82b213b
Rajaram54 <rajaramkumar96@gmail.com>
2021-06-02 08:51:44 -0600

    EXA-28181 | using find instead of for loop

--- app/js/views/claims/index.js
+++ app/js/views/claims/index.js
@@ -3651,13 +3651,9 @@ define(['jquery',
 
                 var can_ahs_pay_to_code = $('#ddlPayToCode').val();
                 var claim_status_id = ~~$('#ddlClaimStatus').val() || null;
-                var claim_Study_date = $('#txtClaimDate').val(); 
-                var facilityTimeZone;
-                
-                app.facilities.forEach(function (val) {
-                    if (val.id === app.default_facility_id) {
-                        facilityTimeZone = val.time_zone
-                    }
+                var claim_Study_date = $('#txtClaimDate').val();
+                var defaultFacility = app.facilities.find(function (val) {
+                    return val.id === app.default_facility_id
                 });
 
                 claim_model.claims = {
@@ -3673,7 +3669,7 @@ define(['jquery',
                     billing_code_id: $('#ddlBillingCode option:selected').val() != '' ? parseInt($('#ddlBillingCode option:selected').val()) : null,
                     billing_class_id: $('#ddlBillingClass option:selected').val() != '' ? parseInt($('#ddlBillingClass option:selected').val()) : null,
                     created_by: app.userID,
-                    claim_dt: $("#txtClaimDate").prop('disabled') ? self.claim_dt_iso :  moment.tz(claim_Study_date, facilityTimeZone).format('YYYY-MM-DD LT z') || null,
+                    claim_dt: $("#txtClaimDate").prop('disabled') ? self.claim_dt_iso :  moment.tz(claim_Study_date, defaultFacility.time_zone).format('YYYY-MM-DD LT z') || null,
                     current_illness_date: $('#txtDate').val() != '' ? commonjs.getISODateString($('#txtDate').val()) : null,
                     same_illness_first_date: $('#txtOtherDate').val() != '' ? commonjs.getISODateString($('#txtOtherDate').val()) : null,
                     unable_to_work_from_date: $('#txtWCF').val() != '' ? commonjs.getISODateString($('#txtWCF').val()) : null,
