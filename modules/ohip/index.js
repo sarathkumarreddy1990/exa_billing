dc3eb5ea5146a33b7b88e3a7e362e5f5ab6b625a
senthilkumara <senthilkumar@emdsys.com>
2021-07-30 15:47:16 +0530

    EXA-29622 | Cron for downloading remittance advice files in billing for OHIP

--- modules/ohip/index.js
+++ modules/ohip/index.js
@@ -272,8 +272,8 @@ const downloadNew = (args, callback) => {
 
         if (resourceIDs && resourceIDs.length && resourceIDs[0]) {
 
-            logger.info(`Downloading Ack Files for  MOH ID ${providerNumber}...`);
-            ebs[EDT_DOWNLOAD]({ providerNumber, resourceIDs }, async (downloadErr, downloadResponse) => {
+            logger.info(`Downloading Ack/RA Files for MOH ID ${providerNumber}...`);
+            await ebs[EDT_DOWNLOAD]({ providerNumber, resourceIDs }, async (downloadErr, downloadResponse) => {
 
                 if (downloadErr) {
                     return callback(downloadErr, null);
@@ -376,6 +376,41 @@ const downloadResponseForProvider = (providerNumber, applicator, resourceType) =
 
 }
 
+const downloadRemittanceForProvider = (providerNumber, resourceType) => {
+    return new Promise((reject, resolve) => {
+        downloadRemittanceAdvice({
+            providerNumber: providerNumber,
+            resourceType: resourceType
+        }, (err, res) => {
+            logger.logInfo(err || res);
+            logger.logInfo(`Completed downloading ${resourceType} files for MOH ID: ${providerNumber}...`);
+
+            return resolve({
+                err,
+                res
+            })
+        });
+    });
+}
+
+const downloadRemittanceFiles = async (providerNumbersList, callback) => {
+    let downloadResults = [];
+
+    for (let i = 0; i < providerNumbersList.length; i++) {
+        logger.logInfo(`Fetching Remittance Advice files for MOH ID: ${providerNumbersList[i].providerNumber}...`);
+
+        try {
+            let response = await downloadRemittanceForProvider(providerNumbersList[i].providerNumber, REMITTANCE_ADVICE);
+            downloadResults.push(response);
+
+        } catch (error) {
+            logger.logError(`Error connecting OHIP Endpoint for MOH ID ${providerNumbersList[i].providerNumber} - ${JSON.stringify(error)}`);
+        }
+    }
+
+    return callback(null, downloadResults);
+}
+
 const downloadSubmittedFiles = async (providerNumbersList, callback) => {
 
     let downloadResourceTypes = [{
@@ -430,7 +465,7 @@ const downloadRemittanceAdvice = async (args, callback) => {
     let {
          providerNumber = ''
     } = args || {};
-    downloadNew({
+    await downloadNew({
             providerNumber,
             resourceType: REMITTANCE_ADVICE
         }, (downloadErr, ediFiles) => {
@@ -439,6 +474,8 @@ const downloadRemittanceAdvice = async (args, callback) => {
                 logger.error(`Error occurred while downloading resource ${REMITTANCE_ADVICE} for  MOH ID: ${providerNumber}`);
                 return callback(downloadErr, []);
             }
+
+            logger.logInfo(`Completed downloading ${REMITTANCE_ADVICE} files for MOH ID: ${providerNumber}...`);
             return callback(downloadErr, ediFiles);
         });
 };
@@ -836,6 +873,8 @@ module.exports = {
     downloadAndProcessResponseFiles,
 
     downloadSubmittedFiles,
+
+    downloadRemittanceFiles,
 
     validateHealthCard: async (args, callback) => {
         const ebs = new EBSConnector((await billingApi.getOHIPConfiguration()).ebsConfig);
---
+++ modules/ohip/raDownloads.js
@@ -0,0 +1,66 @@
+const logger = require('../../logger');
+const cronJob = require('cron').CronJob;
+const ohip = require('./index');
+const shared = require('../../server/shared');
+
+const SERVICE_NAME = process.env.SERVICE_NAME || `OHIP Remittance Advice Files`;
+class RaDownloads {
+
+    constructor(config) {
+        config = config || {};
+        this.cronExpression = config.cron || '*/10 * * * *';
+        this.inProgress = false;
+        this.restartInterval = config.interval || 60000; /// in milliseconds
+    }
+
+    async start() {
+        logger.info(` Initialized ${process.env.SERVICE_NAME} service`);
+
+        let company_id = await shared.getCompanyId();
+
+        if (!company_id) {
+            logger.logError(`[${SERVICE_NAME}] - No company_id to use for connection to billing - `, e);
+            return false;
+        }
+
+        new cronJob(this.cronExpression, async () => {
+            if (this.inProgress) {
+
+                logger.logInfo(`${SERVICE_NAME} still running for downloading RA files for old batch of providers!`);
+                return;
+            }
+
+            this.inProgress = true;
+            this.cronTicks = 0;
+            logger.info('Started Fetching Details....');
+
+            let providerNumbersList = await shared.getProviderNumbers(company_id);
+
+            if (!providerNumbersList.length) {
+                logger.logError(`[${SERVICE_NAME}] - No Provider Numbers available for the providers to continue...`);
+                this.inProgress = false;
+                return false;
+            }
+
+            try {
+                await ohip.downloadRemittanceFiles(providerNumbersList, (err, res) => {
+
+                    if (err) {
+                        logger.error(`Error in downloading remittance advice files ${err}`);
+                    }
+
+                    logger.logInfo(`${process.env.SERVICE_NAME} service completed downloading remittance advice files`);
+                    this.inProgress = false;
+                });
+
+            } catch (error) {
+                logger.logError(`Error occured in downloading ack files ${error}`);
+                this.inProgress = false;
+                return false;
+            }
+
+        }, null, true);
+    }
+}
+
+module.exports = RaDownloads;
