c27ed6cdaee5f9b6bda65f73081b631413a84287
megavarnan <megavarnanmegam14@gmail.com>
2021-08-04 18:08:42 +0530

    EXA-30255 | Fix payer name in payment

--- modules/reporting/data/postgres/billing/payment-report.js
+++ modules/reporting/data/postgres/billing/payment-report.js
@@ -240,7 +240,7 @@ const detailQueryTemplate = _.template(`
     LEFT JOIN public.Providers pr ON pr.id = pc.provider_id
     LEFT JOIN public.patients pp ON pp.id = c.patient_id
     LEFT JOIN public.patients p_pp ON p_pp.id = p.patient_id
-    LEFT JOIN public.ordering_facility_contacts pofc ON pofc.id = pd.ordering_facility_contact_id
+    LEFT JOIN public.ordering_facility_contacts pofc ON pofc.ordering_facility_id = p.ordering_facility_id
     LEFT JOIN public.ordering_facilities pof ON pof.id = pofc.ordering_facility_id
     <% if(insGroups) { %>
        LEFT JOIN public.ordering_facilities pofs ON pofs.id = p.ordering_facility_id
