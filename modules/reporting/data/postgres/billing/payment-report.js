cbea2e75cc77bbbf9c30d7dd0782b1c5c67cba0c
megavarnan <megavarnanmegam14@gmail.com>
2021-08-05 23:06:28 +0530

    EXA-32055 | Fix payer name in report : update() ordering facility

--- modules/reporting/data/postgres/billing/payment-report.js
+++ modules/reporting/data/postgres/billing/payment-report.js
@@ -132,7 +132,6 @@ const detailQueryTemplate = _.template(`
             SUM(CASE WHEN amount_type= 'adjustment' then bpa.amount  else 0::money end) AS adjustment,
             ARRAY_REMOVE(ARRAY_AGG(DISTINCT bac.description),null) AS description
             <% if (userIds) { %> , MAX(users.username) AS user_name  <% } %>
-            , bp.ordering_facility_id
         FROM
             billing.payments bp
         LEFT JOIN billing.payment_applications bpa ON bpa.payment_id = bp.id
@@ -240,7 +239,7 @@ const detailQueryTemplate = _.template(`
     LEFT JOIN public.Providers pr ON pr.id = pc.provider_id
     LEFT JOIN public.patients pp ON pp.id = c.patient_id
     LEFT JOIN public.patients p_pp ON p_pp.id = p.patient_id
-    LEFT JOIN public.ordering_facilities pofs ON pofs.id = pd.ordering_facility_id
+    LEFT JOIN public.ordering_facilities pofs ON pofs.id = p.ordering_facility_id
     <% if(insGroups) { %>
        LEFT JOIN  insurance_provider_payer_types ippt ON ippt.id = ip.provider_payer_type_id
     <% } %>
