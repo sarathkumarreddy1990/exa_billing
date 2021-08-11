9405a203dc323252acb3ce2b67f6e2d5391a7851
megavarnan <megavarnanmegam14@gmail.com>
2021-08-11 14:50:19 +0530

    EXA-30321 | service facility monthly recap report :update()

--- modules/reporting/data/postgres/billing/monthly-recap.js
+++ modules/reporting/data/postgres/billing/monthly-recap.js
@@ -289,10 +289,10 @@ const api = {
 
         // Facility Filter
         if (params.allFacilities && params.facilityIds)
-            filtersUsed.push({ name: 'facilities', label: 'Facilities', value: 'All' });
+            filtersUsed.push({ name: 'serviceFacilities', label: 'Service Facilities', value: 'All' });
         else {
             const facilityNames = _(lookups.servicefacilities).filter(f => params.facilityIds && params.facilityIds.map(Number).indexOf(parseInt(f.id, 10)) > -1).map(f => f.name).value();
-            filtersUsed.push({ name: 'facilities', label: 'Facilities', value: facilityNames });
+            filtersUsed.push({ name: 'serviceFacilities', label: 'Service Facilities', value: facilityNames });
         }
         // Billing provider Filter
         if (params.allBillingProvider == 'true')
