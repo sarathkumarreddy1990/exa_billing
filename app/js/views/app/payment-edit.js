3fe57fc8713f4ffeb97e85afdce7e7b2fafcc84e
Saranya <saranya.l@emdsys.com>
2021-08-19 21:08:54 +0530

    EXA-29331 | Claim Status is changing when a patient payment is applied to a claim

--- app/js/views/app/payment-edit.js
+++ app/js/views/app/payment-edit.js
@@ -2508,9 +2508,8 @@ define(['jquery',
                     var claimStatus = _.filter(self.claimStatuses.toJSON(), { id: self.received_claim_status_id });
                     var oldClaimStatus = claimStatus.length && claimStatus[0].code || '';
                     var isClaimStatusChanged = self.received_claim_status_id != $('#ddlClaimStatus').val();
-                    var claimStatusIndex = ['PV', 'PS'].indexOf(oldClaimStatus);
 
-                    if (totalPayment === 0 && totalAdjustment === 0 && claimStatusIndex === -1 && paymentPayerType !=='patient') {
+                    if (totalPayment === 0 && totalAdjustment === 0 && paymentPayerType !=='patient') {
                         var deniedStatus = _.filter(self.claimStatuses.toJSON(), { code: 'D' });
                         $('#ddlClaimStatus').val(deniedStatus.length && deniedStatus[0].id || '');
                         isClaimDenied = true;
@@ -2525,6 +2524,12 @@ define(['jquery',
                     var coPay = $('#txtCoPay').val();
                     var claimStatusID = self.received_claim_status_id != $('#ddlClaimStatus').val() ? $('#ddlClaimStatus').val()
                         : isClaimDenied ? $('#ddlClaimStatus').val() : 0;
+                    var totalClaimBalance = self.currentOrderBalance && parseFloat(self.currentOrderBalance.replace('$', ''));
+                    var isClaimBalance = true;
+
+                    if (totalClaimBalance >= 0 && totalPayment >= totalClaimBalance) {
+                        isClaimBalance = false;
+                    }
 
                     commonjs.showLoading();
                     targetObj.attr('disabled', true);
@@ -2546,11 +2551,11 @@ define(['jquery',
                             adjustmentId: adjustmentType,
                             paymentStatus: paymentStatus,
                             casDeleted: JSON.stringify(self.casDeleted),
-                            claimStatusID: self.currentResponsible === 'primary_insurance' && claimStatusIndex > -1 && !isClaimStatusChanged && paymentPayerType === 'patient' ? self.received_claim_status_id : claimStatusID,
+                            claimStatusID: !isClaimStatusChanged && isClaimBalance && paymentPayerType === 'patient' ? self.received_claim_status_id : claimStatusID,
                             is_payerChanged: isPayerChanged,
                             is_claimDenied: isClaimDenied,
                             isFromClaim: self.isFromClaim,
-                            changeResponsibleParty : self.currentResponsible === 'primary_insurance' && paymentPayerType === 'patient' && claimStatusIndex > -1 && isPayerChanged === 'false' && !isClaimStatusChanged
+                            changeResponsibleParty : paymentPayerType === 'patient' && isPayerChanged === 'false' && !isClaimStatusChanged && isClaimBalance
                         },
                         success: function (model, response) {
                             var msg = self.isFromClaim ? commonjs.geti18NString('messages.status.tosSuccessfullyCompleted') :
