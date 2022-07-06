4efbf9e5d00c867727a0a574f624a6000ef14433
Esther <easther.yesuvadiyan@konicaminolta.com>
2022-07-06 19:23:55 +0530

    EXA-35882 | As per AC request, the skill code should save for all responsible parties

--- app/js/views/claims/index.js
+++ app/js/views/claims/index.js
@@ -4082,7 +4082,7 @@ define(['jquery',
             getSkillCodeId: function(currentPayer_type) {
                 var can_ahs_skill_code_id = null;
 
-                if (app.billingRegionCode === 'can_AB' && currentPayer_type === "PIP" && (this.priInsCode === "WCB" || this.priInsCode === "AHS")) {
+                if (app.billingRegionCode === 'can_AB' && (this.priInsCode === "WCB" || this.priInsCode === "AHS")) {
                     can_ahs_skill_code_id = this.ACSelect && this.ACSelect.skillCodes ? this.ACSelect.skillCodes.ID : null;
                 }
 
