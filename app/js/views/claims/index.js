692bf0e5078592afe5abe4c3ef9a1d84a5939121
Senthilkumar <senthilkumar@emdsys.com>
2023-02-02 16:21:21 +0530

    EXA-35895 | Rendering provider validation is added to claim creation for all canadian provinces

--- app/js/views/claims/index.js
+++ app/js/views/claims/index.js
@@ -4532,7 +4532,7 @@ define(['jquery',
                     return false;
                 }
 
-                if ( !self.ACSelect.readPhy.contact_id && app.billingRegionCode === 'can_AB') {
+                if (!self.ACSelect.readPhy.contact_id && app.country_alpha_3_code === 'can') {
                     commonjs.showWarning("messages.warning.shared.selectRenderingProvider");
                     $('#ddlRenderingProvider').focus();
                     return false;
