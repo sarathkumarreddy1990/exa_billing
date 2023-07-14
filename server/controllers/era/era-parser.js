eb60bd4d432aa785514d0e7f84e82ec1023f72b2
wilson_novido <wnovido@viztek.net>
2023-07-14 11:18:14 -0400

    EXA-40196 | Denied claim in processing EOB file

--- server/controllers/era/era-parser.js
+++ server/controllers/era/era-parser.js
@@ -209,6 +209,7 @@ module.exports = {
                         */
                         let cas_obj = [];
                         let isGroupDeniedStatus = false;
+                        let hasCASPatientResponsibility = false;
 
                         _.each(validCAS, function (cas) {
 
@@ -222,6 +223,11 @@ module.exports = {
                                     if (cas.groupCode === 'PR' && cas['reasonCode' + j] === '96') {
                                         isGroupDeniedStatus = true;
                                     }
+
+                                    if (cas.groupCode === 'PR' && cas['reasonCode' + j] !== '96') {
+                                        hasCASPatientResponsibility = true;
+                                    }
+
                                     cas_obj.push({
                                         group_code_id: groupcode && groupcode.length ? groupcode[0].id : null,
                                         reason_code_id: reasoncode && reasoncode.length ? reasoncode[0].id : null,
@@ -292,7 +298,8 @@ module.exports = {
                             is_debit: isDebit,
                             code: adjustment_code,
                             claim_index: claim_index,
-                            is_group_denied: isGroupDeniedStatus
+                            is_group_denied: isGroupDeniedStatus,
+                            has_cas_patient_responsibility: hasCASPatientResponsibility
                         });
                     });
 
@@ -382,8 +389,9 @@ module.exports = {
             }
 
             const is_denied_claim_status = items.some(val =>
-                (val.payment === 0
+                ((val.payment === 0
                 && (val.adjustment === 0 || val.adjustment === val.bill_fee))
+                && !val.has_cas_patient_responsibility)
                 || val.is_group_denied
             );
 
