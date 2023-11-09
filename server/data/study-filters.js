babdc48e59a10e718a9a72c327e53c01c18b8106
Senthilkumar <senthilkumar@emdsys.com>
2023-11-09 16:03:22 +0530

    EXA-42042 | Fetch user selected facilties in web and billing worklist and user filters

--- server/data/study-filters.js
+++ server/data/study-filters.js
@@ -22,39 +22,43 @@ module.exports = {
 
     getUserWLFilters: async function (args) {
         let sqlQuery;
+        const filterType = args.flag === 'claim_workbench'
+            ? 'claims'
+            : 'studies';
 
-        if(args.id > 0) {
-            sqlQuery = SQL `
-                        SELECT
-                            filter_info  AS perms_filter,
-                            grid_filters.*
-                        FROM
-                            billing.grid_filters
-                        WHERE
-                            grid_filters.id = ${args.id} `;
+        sqlQuery = SQL`
+            SELECT
+                us.worklist_filter_info AS perms_filter,
+                bgf.filter_info AS joined_filter_info,
+                bus.grid_field_settings AS grid_options,
+                row_to_json(u.*) AS user_details,
+                bgf.*
+            FROM public.user_settings us
+            JOIN LATERAL (
+                SELECT
+                    user_type,
+                    facilities,
+                    default_facility_id,
+                    all_facilities,
+                    user_group_id
+                FROM users
+                WHERE id = us.user_id
+            ) u ON TRUE
+            LEFT JOIN billing.grid_filters bgf ON bgf.user_id = us.user_id
+            LEFT JOIN billing.user_settings bus ON bus.user_id = us.user_id
+            WHERE
+                us.user_id = ${args.user_id}
+                AND CASE
+                        WHEN ${args.id} > 0
+                        THEN (
+                            bgf.id = ${args.id}
+                            AND bgf.filter_type = ${filterType}
+                            AND bus.grid_name = ${filterType}
+                        )
+                        ELSE bus.grid_name = ${filterType}
+                    END
+        `;
 
-        } else {
-            sqlQuery = SQL`
-                        SELECT
-                            worklist_filter_info    AS perms_filter,
-                            row_to_json(u.*)        AS user_details
-                        FROM
-                            public.user_settings
-                        JOIN LATERAL (
-                            SELECT
-                                user_type,
-                                facilities,
-                                default_facility_id,
-                                all_facilities,
-                                user_group_id
-                            FROM users
-                            WHERE id = user_id
-                        ) u ON TRUE
-                        WHERE
-                            user_id = ${args.user_id}
-            `;
-        }
-        
         return await query(sqlQuery);
     }
 };
